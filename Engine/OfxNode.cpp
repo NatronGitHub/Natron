85ea534a53792c7f1506bacb0d4f7fb764be248f
Frédéric Devernay <frederic.devernay@m4x.org>
2013-09-19 18:21:57 +0200

    fix qmime.h warning

--- Engine/OfxNode.cpp
+++ Engine/OfxNode.cpp
@@ -13,6 +13,11 @@
 
 #include <locale>
 #include <QImage>
+#if QT_VERSION < 0x050000
+CLANG_DIAG_OFF(unused-private-field);
+#include <QtGui/qmime.h>
+CLANG_DIAG_ON(unused-private-field);
+#endif
 #include <QKeyEvent>
 
 #include "Global/AppManager.h"
