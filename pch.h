fff611e86df73c4169af8ce0d3da826868ff2de6
Frédéric Devernay <frederic.devernay@m4x.org>
2013-11-04 11:52:58 +0100

    add precompiled header

---
+++ pch.h
@@ -0,0 +1,15 @@
+#ifndef PCH_H
+#define PCH_H
+
+#include <vector>
+#include <string>
+#include <map>
+
+#include <QtCore>
+#include <QtGui>
+
+#include <boost/utility.hpp>
+#include <boost/shared_ptr.hpp>
+#include <boost/scoped_ptr.hpp>
+
+#endif // PCH_H
