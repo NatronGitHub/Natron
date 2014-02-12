91fac86050f294ce41145bab9fa442f6acb5b8a8
Frédéric Devernay <frederic.devernay@m4x.org>
2014-02-12 15:04:40 +0100

    fix warning

--- Gui/AnimationButton.cpp
+++ Gui/AnimationButton.cpp
@@ -6,7 +6,11 @@
 
 #include "AnimationButton.h"
 
+#include "Global/Macros.h"
+CLANG_DIAG_OFF(unused-private-field)
+// /opt/local/include/QtGui/qmime.h:119:10: warning: private field 'type' is not used [-Wunused-private-field]
 #include <QMouseEvent>
+CLANG_DIAG_ON(unused-private-field)
 #include <QApplication>
 #include <QDragEnterEvent>
 #include <QDragLeaveEvent>
