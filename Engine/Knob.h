afd2fa3cfbb83a636269f11f7d15e92bc0d24cfb
Frédéric Devernay <frederic.devernay@m4x.org>
2014-03-08 17:50:38 +0100

    Knob.h: WARN_UNUSED_RETURN (and get one more warning)

--- Engine/Knob.h
+++ Engine/Knob.h
@@ -52,19 +52,19 @@ public:
      * @brief Must return the type name of the knob. This name will be used by the KnobFactory
      * to create an instance of this knob.
     **/
-    virtual const std::string& typeName() const = 0;
+    virtual const std::string& typeName() const WARN_UNUSED_RETURN = 0;
 
     /**
      * @brief Must return true if this knob can animate (i.e: if we can set different values depending on the time)
      * Some parameters cannot animate, for example a file selector.
      **/
-    virtual bool canAnimate() const = 0;
+    virtual bool canAnimate() const WARN_UNUSED_RETURN = 0;
 
     /**
       * @brief If the parameter is multidimensional, this is the label thats the that will be displayed
       * for a dimension.
     **/
-    virtual std::string getDimensionName(int dimension) const {(void)dimension; return "";}
+    virtual std::string getDimensionName(int dimension) const WARN_UNUSED_RETURN {(void)dimension; return "";}
 
     /**
      * @brief Used to bracket calls to setValue. This indicates than a series of calls will be made, and
@@ -111,13 +111,13 @@ public:
      * @brief Called by save(KnobSerialization*) if you want to serialize more data.
      * You must return a string with your serialized data
      **/
-    virtual QString saveExtraData() const { return ""; }
+    virtual QString saveExtraData() const WARN_UNUSED_RETURN { return ""; }
     
 
-    Variant getValue(int dimension = 0) const;
+    Variant getValue(int dimension = 0) const WARN_UNUSED_RETURN;
 
     template <typename T>
-    T getValue(int dimension = 0) const {
+    T getValue(int dimension = 0) const WARN_UNUSED_RETURN {
         return getValue(dimension).value<T>();
     }
 
@@ -164,10 +164,10 @@ public:
      * @brief Returns the value  in a specific dimension at a specific time. If
      * there is no key in this dimension it will return the value at the requested dimension
      **/
-    virtual Variant getValueAtTime(double time, int dimension) const;
+    virtual Variant getValueAtTime(double time, int dimension) const WARN_UNUSED_RETURN;
 
     template<typename T>
-    T getValueAtTime(double time,int dimension = 0) const {
+    T getValueAtTime(double time,int dimension = 0) const WARN_UNUSED_RETURN {
         return getValueAtTime(time,dimension).value<T>();
     }
     
@@ -175,50 +175,50 @@ public:
      * @brief Places in time the keyframe time at the given index.
      * If it exists the function returns true, false otherwise.
      **/
-    bool getKeyFrameTime(int index,int dimension,double* time) const;
+    bool getKeyFrameTime(int index,int dimension,double* time) const WARN_UNUSED_RETURN;
     
     /**
      * @brief Convenience function, does the same as getKeyFrameByIndex but returns the last
      * keyframe.
      **/
-    bool getLastKeyFrameTime(int dimension,double* time) const;
+    bool getLastKeyFrameTime(int dimension,double* time) const WARN_UNUSED_RETURN;
     
     /**
      * @brief Convenience function, does the same as getKeyFrameByIndex but returns the first
      * keyframe.
      **/
-    bool getFirstKeyFrameTime(int dimension,double* time) const;
+    bool getFirstKeyFrameTime(int dimension,double* time) const WARN_UNUSED_RETURN;
     
-    int getKeyFramesCount(int dimension) const;
+    int getKeyFramesCount(int dimension) const WARN_UNUSED_RETURN;
     
     /**
      * @brief Returns the nearest keyframe time if it was found.
      * Returns true if it succeeded, false otherwise.
      **/
-    bool getNearestKeyFrameTime(int dimension,double time,double* nearestTime) const;
+    bool getNearestKeyFrameTime(int dimension,double time,double* nearestTime) const WARN_UNUSED_RETURN;
     
     /**
      * @brief Returns the keyframe index if there's any keyframe in the curve 
      * at the given dimension and the given time. -1 otherwise.
      **/
-    int getKeyFrameIndex(int dimension, double time) const;
+    int getKeyFrameIndex(int dimension, double time) const WARN_UNUSED_RETURN;
     
-    bool getKeyFrameValueByIndex(int dimension,int index,Variant* value) const;
+    bool getKeyFrameValueByIndex(int dimension,int index,Variant* value) const WARN_UNUSED_RETURN;
 
 
-    boost::shared_ptr<Curve> getCurve(int dimension = 0) const;
+    boost::shared_ptr<Curve> getCurve(int dimension = 0) const WARN_UNUSED_RETURN;
 
     /**
      * @brief Returns true if the dimension is animated with keyframes.
      **/
-    bool isAnimated(int dimension) const;
+    bool isAnimated(int dimension) const WARN_UNUSED_RETURN;
     
     /**
      * @brief Returns true if at least 1 dimension is animated.
      **/
-    bool hasAnimation() const;
+    bool hasAnimation() const WARN_UNUSED_RETURN;
 
-    const std::vector< boost::shared_ptr<Curve>  >& getCurves() const;
+    const std::vector< boost::shared_ptr<Curve>  >& getCurves() const WARN_UNUSED_RETURN;
 
     void setAnimationEnabled(bool val) ;
 
@@ -226,11 +226,11 @@ public:
      * @brief Returns true if the animation is enabled for this knob. A return value of
      * true doesn't necessarily means that the knob is animated at all.
      **/
-    bool isAnimationEnabled() const;
+    bool isAnimationEnabled() const WARN_UNUSED_RETURN;
 
-    const std::string& getDescription() const;
+    const std::string& getDescription() const WARN_UNUSED_RETURN;
 
-    KnobHolder* getHolder() const;
+    KnobHolder* getHolder() const WARN_UNUSED_RETURN;
 
     /**
      * @brief Called by a render task to copy all the knobs values to another separate instance
@@ -239,13 +239,13 @@ public:
     **/
     void cloneValue(const Knob& other);
 
-    const std::vector<Variant>& getValueForEachDimension() const ;
+    const std::vector<Variant>& getValueForEachDimension() const WARN_UNUSED_RETURN;
 
-    int getDimension() const ;
+    int getDimension() const WARN_UNUSED_RETURN;
 
     void turnOffNewLine();
     
-    bool isNewLineTurnedOff() const;
+    bool isNewLineTurnedOff() const WARN_UNUSED_RETURN;
     
     void setSpacingBetweenItems(int spacing);
     
@@ -260,33 +260,33 @@ public:
      name is the same as the description(i.e: the text label).*/
     void setName(const std::string& name);
     
-    std::string getName() const;
+    std::string getName() const WARN_UNUSED_RETURN;
     
     void setParentKnob(boost::shared_ptr<Knob> knob);
     
-    boost::shared_ptr<Knob> getParentKnob() const;
+    boost::shared_ptr<Knob> getParentKnob() const WARN_UNUSED_RETURN;
     
-    int determineHierarchySize() const;
+    int determineHierarchySize() const WARN_UNUSED_RETURN;
     
-    bool getIsSecret() const ;
+    bool getIsSecret() const WARN_UNUSED_RETURN;
     
-    bool isEnabled(int dimension) const;
+    bool isEnabled(int dimension) const WARN_UNUSED_RETURN;
     
     void setEvaluateOnChange(bool b);
     
-    bool getIsPersistant() const;
+    bool getIsPersistant() const WARN_UNUSED_RETURN;
 
     void setIsPersistant(bool b);
 
     void setCanUndo(bool val);
     
-    bool getCanUndo() const;
+    bool getCanUndo() const WARN_UNUSED_RETURN;
     
-    bool getEvaluateOnChange() const;
+    bool getEvaluateOnChange() const WARN_UNUSED_RETURN;
     
     void setHintToolTip(const std::string& hint);
     
-    const std::string& getHintToolTip() const;
+    const std::string& getHintToolTip() const WARN_UNUSED_RETURN;
 
     /**
      * @brief Slaves the value for the given dimension to the curve
@@ -306,15 +306,15 @@ public:
      * @brief Returns a valid pointer to a knob if the value at
      * the given dimension is slaved.
     **/
-    std::pair<int,boost::shared_ptr<Knob> > getMaster(int dimension) const;
+    std::pair<int,boost::shared_ptr<Knob> > getMaster(int dimension) const WARN_UNUSED_RETURN;
 
     /**
      * @brief Returns true if the value at the given dimension is slave to another parameter
      **/
-    bool isSlave(int dimension) const;
+    bool isSlave(int dimension) const WARN_UNUSED_RETURN;
     
 
-    const std::vector<std::pair<int,boost::shared_ptr<Knob> > > &getMasters() const;
+    const std::vector<std::pair<int,boost::shared_ptr<Knob> > > &getMasters() const WARN_UNUSED_RETURN;
 
     /**
      * @brief Called by the GUI whenever the animation level changes (due to a time change
@@ -322,7 +322,7 @@ public:
      **/
     void setAnimationLevel(int dimension,Natron::AnimationLevel level);
     
-    Natron::AnimationLevel getAnimationLevel(int dimension) const;
+    Natron::AnimationLevel getAnimationLevel(int dimension) const WARN_UNUSED_RETURN;
 
     
     /*Set the value of the knob but does NOT emit the valueChanged signal.
@@ -350,7 +350,7 @@ public:
     void resetToDefaultValue(int dimension);
     
     
-    virtual bool isTypeCompatible(const Knob& other) const = 0;
+    virtual bool isTypeCompatible(const Knob& other) const WARN_UNUSED_RETURN = 0;
 public slots:
     
   
@@ -452,7 +452,7 @@ private:
      * This function is almost only addressing the problem of animating strings. The string knob just returns
      * in the return value the string at the given index contained in interpolated.
      **/
-    virtual void variantFromInterpolatedValue(double interpolated,Variant* returnValue) const ;
+    virtual void variantFromInterpolatedValue(double interpolated,Variant* returnValue) const;
 
 private:
     struct KnobPrivate;
@@ -486,12 +486,12 @@ public:
     
     virtual ~KnobHolder();
     
-    bool isClone() const { return _isClone; }
+    bool isClone() const WARN_UNUSED_RETURN { return _isClone; }
 
     void setClone() { _isClone = true; }
     
     template<typename K>
-    boost::shared_ptr<K> createKnob(const std::string &description, int dimension = 1) const {
+    boost::shared_ptr<K> createKnob(const std::string &description, int dimension = 1) const WARN_UNUSED_RETURN {
         return Natron::createKnob<K>(this, description,dimension);
     }
     
@@ -501,13 +501,13 @@ public:
      **/
     void cloneKnobs(const KnobHolder& other);
 
-    AppInstance* getApp() const {return _app;}
+    AppInstance* getApp() const WARN_UNUSED_RETURN {return _app;}
 
-    int getAppAge() const;
+    int getAppAge() const WARN_UNUSED_RETURN;
     
     boost::shared_ptr<Knob> getKnobByDescription(const std::string& desc) const WARN_UNUSED_RETURN;
     
-    const std::vector< boost::shared_ptr<Knob> >& getKnobs() const { return _knobs; }
+    const std::vector< boost::shared_ptr<Knob> >& getKnobs() const WARN_UNUSED_RETURN { return _knobs; }
 
     void refreshAfterTimeChange(SequenceTime time);
     
