de395e54184943b31a67941f4157a33f8bb169ae
Frédéric Devernay <frederic.devernay@m4x.org>
2014-03-10 15:24:06 +0100

    update openfx & GitVersion

--- Global/GitVersion.h
+++ Global/GitVersion.h
@@ -1,5 +1,5 @@
 #ifndef NATRON_GITVERSION_H_
 #define NATRON_GITVERSION_H_
 #define GIT_BRANCH "workshop"
-#define GIT_COMMIT "7a30ddc8d4a139d5b30dd44aa4ccd8f69120fff7"
+#define GIT_COMMIT "344a6a3bbc7dca6b64068eb8f0b39bd88404d910"
 #endif
