105bd4fa9b589f6ea19615f1f3f1900ebf1e1e88
MrKepzie <alexandre230@msn.com>
2014-03-12 18:05:17 +0100

    update git version

--- Global/GitVersion.h
+++ Global/GitVersion.h
@@ -1,5 +1,5 @@
 #ifndef NATRON_GITVERSION_H_
 #define NATRON_GITVERSION_H_
 #define GIT_BRANCH "workshop"
-#define GIT_COMMIT "d69f6e4120a1743a5dfcff573c747737e2f9e27e"
+#define GIT_COMMIT "d1d3d2b4489db68e599f308c87dadea47f7479c9"
 #endif
