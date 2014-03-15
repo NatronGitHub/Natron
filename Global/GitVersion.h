da44b6f707aa350651dcf4a39037b3328b56189f
MrKepzie <alexandre230@msn.com>
2014-03-15 10:58:55 +0100

    Git version & update OpenFX

--- Global/GitVersion.h
+++ Global/GitVersion.h
@@ -1,5 +1,5 @@
 #ifndef NATRON_GITVERSION_H_
 #define NATRON_GITVERSION_H_
 #define GIT_BRANCH "workshop"
-#define GIT_COMMIT "a5fbb542ce55a97dbd303efa25ba9cb3baed4ce2"
+#define GIT_COMMIT "83404bb0e928de0d16a2505c6bfd599815bfab99"
 #endif
