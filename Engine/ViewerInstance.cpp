4a40b47c7bf261f8308856ff708a874535bfb8a6
MrKepzie <immarespond>
2014-03-19 09:14:52 +0100

    Fix includes of QtConcurrentMap

--- Engine/ViewerInstance.cpp
+++ Engine/ViewerInstance.cpp
@@ -16,7 +16,7 @@
 
 CLANG_DIAG_OFF(deprecated)
 #include <QtCore/QtGlobal>
-#include <QtCore/QtConcurrentMap>
+#include <QtConcurrentMap>
 #include <QtCore/QFutureWatcher>
 #include <QtCore/QMutex>
 #include <QtCore/QWaitCondition>
