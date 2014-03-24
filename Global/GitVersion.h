e80e22b25911a8e1c95b29879d097c2c8d13d144
MrKepzie <alexandre230@msn.com>
2014-03-24 16:03:42 +0100

    Nicer error when memory allocation failure

--- Global/GitVersion.h
+++ Global/GitVersion.h
@@ -1,5 +1,5 @@
 #ifndef NATRON_GITVERSION_H_
 #define NATRON_GITVERSION_H_
 #define GIT_BRANCH "workshop"
-#define GIT_COMMIT "caaf5abdce7fb8cd0372bad371c03fb0dae93ee5"
+#define GIT_COMMIT "0104ca7528b3fac376e469c08c0d24b5aa0bf264"
 #endif
