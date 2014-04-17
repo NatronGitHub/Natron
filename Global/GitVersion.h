63abc9339f46393e4a29a236bd2722e2407c3ff6
MrKepzie <alexandre230@msn.com>
2014-04-17 16:33:20 +0200

    Never use Image::scale, this has partly fixed the render scale

--- Global/GitVersion.h
+++ Global/GitVersion.h
@@ -1,5 +1,5 @@
 #ifndef NATRON_GITVERSION_H_
 #define NATRON_GITVERSION_H_
 #define GIT_BRANCH "workshop"
-#define GIT_COMMIT "20a9af467cbb9634e24e208add665cb88ddb7feb"
+#define GIT_COMMIT "cf44999d9bdf9ec006135e454d3614ab59e5354b"
 #endif
