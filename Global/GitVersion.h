e14ec33eaf30e4bffa780b35fa422117e39ba8dc
MrKepzie <alexandre230@msn.com>
2014-04-23 11:25:45 +0200

    update sequence parsing

--- Global/GitVersion.h
+++ Global/GitVersion.h
@@ -1,5 +1,5 @@
 #ifndef NATRON_GITVERSION_H_
 #define NATRON_GITVERSION_H_
 #define GIT_BRANCH "workshop"
-#define GIT_COMMIT "93bbee342ec009b5ea89b9422435d4babebe2558"
+#define GIT_COMMIT "5a759541aa268788c57113ae6c3919b04b3219ac"
 #endif
