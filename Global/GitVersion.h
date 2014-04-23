f000a64759395207fc16d9375e419889401a3cf7
MrKepzie <alexandre230@msn.com>
2014-04-23 13:57:39 +0200

    Fix gui.pro

--- Global/GitVersion.h
+++ Global/GitVersion.h
@@ -1,5 +1,5 @@
 #ifndef NATRON_GITVERSION_H_
 #define NATRON_GITVERSION_H_
 #define GIT_BRANCH "workshop"
-#define GIT_COMMIT "5c23425c24edf459316c88600befafc57937765d"
+#define GIT_COMMIT "cd531de68652178f3dd6ec42cf4bb615d041b1f0"
 #endif
