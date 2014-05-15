b62a4bcc7f8b0f98f4ac487b663ad31e3789e6c9
Frédéric Devernay <frederic.devernay@m4x.org>
2014-05-15 18:22:58 +0200

    fix more warnings

--- Gui/RotoPanel.h
+++ Gui/RotoPanel.h
@@ -10,7 +10,12 @@
 #include <boost/shared_ptr.hpp>
 #include <boost/scoped_ptr.hpp>
 
+#include "Global/Macros.h"
+CLANG_DIAG_OFF(deprecated)
+CLANG_DIAG_OFF(uninitialized)
 #include <QWidget>
+CLANG_DIAG_ON(deprecated)
+CLANG_DIAG_ON(uninitialized)
 
 #include "Global/GlobalDefines.h"
 
