aae4793f5b0a42f7f1d4a6c2359ce7a0a09122ba
MrKepzie <alexandre230@msn.com>
2014-05-25 12:54:16 +0200

    Fixed missing condition in getImage

--- Global/GitVersion.h
+++ Global/GitVersion.h
@@ -1,5 +1,5 @@
 #ifndef NATRON_GITVERSION_H_
 #define NATRON_GITVERSION_H_
 #define GIT_BRANCH "workshop"
-#define GIT_COMMIT "96123b1671746d7fcbbdcfb4ed76ef28780591b2"
+#define GIT_COMMIT "08a9db73bc897d36565821095abf32d38a99aac8"
 #endif
