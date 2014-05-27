1be85456f8b36b8e571bb0216f3fb7814b82fdaa
MrKepzie <immarespond>
2014-05-27 15:54:48 +0200

    Don't delete gui

--- Gui/GuiAppInstance.cpp
+++ Gui/GuiAppInstance.cpp
@@ -73,10 +73,10 @@ void GuiAppInstance::aboutToQuit()
 
 GuiAppInstance::~GuiAppInstance() {
     
-#ifndef __NATRON_WIN32__
-    delete _imp->_gui;
-    _imp->_gui = 0;
-#endif
+//#ifndef __NATRON_WIN32__
+//    delete _imp->_gui;
+//    _imp->_gui = 0;
+//#endif
     QCoreApplication::processEvents();
 
 }
