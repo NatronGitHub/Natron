1842e231fa74907ef0016760577b6f72d1c2d148
MrKepzie <alexandre230@msn.com>
2014-05-30 16:32:26 +0200

    Update sequence parsing. The file dialog is slightly more performant as a result of it.

--- Global/GitVersion.h
+++ Global/GitVersion.h
@@ -1,5 +1,5 @@
 #ifndef NATRON_GITVERSION_H_
 #define NATRON_GITVERSION_H_
 #define GIT_BRANCH "workshop"
-#define GIT_COMMIT "d32658bb4159c0760be2c01db542cf0cac53701a"
+#define GIT_COMMIT "6d25885402aaae99808ce5b800239a5a627e4a41"
 #endif
