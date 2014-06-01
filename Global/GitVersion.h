62b3bf35115c3938376ab200f0df44554bb7ddbd
MrKepzie <alexandre230@msn.com>
2014-06-01 10:17:03 +0200

    Move to config.pri Xcode stuff

--- Global/GitVersion.h
+++ Global/GitVersion.h
@@ -1,5 +1,5 @@
 #ifndef NATRON_GITVERSION_H_
 #define NATRON_GITVERSION_H_
 #define GIT_BRANCH "workshop"
-#define GIT_COMMIT "e1d0ca935cb7fd766de78105d2c30ca432446c42"
+#define GIT_COMMIT "465dab05220e218ce16aaab0b054f0cb7b502918"
 #endif
