118e00903525716aa7eb8e226be223ff4b0e2b8e
MrKepzie <alexandre230@msn.com>
2014-06-18 22:59:38 +0200

    Natron: flag for version 0.93

--- Global/GitVersion.h
+++ Global/GitVersion.h
@@ -1,5 +1,5 @@
 #ifndef NATRON_GITVERSION_H_
 #define NATRON_GITVERSION_H_
 #define GIT_BRANCH "workshop"
-#define GIT_COMMIT "8b6f3c5a2277222dcc994a0fed74844d52f5c423"
+#define GIT_COMMIT "b75f04731e3dec5fc743f7f50389c14cc3e61b17"
 #endif
