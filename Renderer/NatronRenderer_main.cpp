e3b883f3578ba7baf5c208a98d02de6604db2d9f
Frédéric Devernay <frederic.devernay@m4x.org>
2014-06-19 17:37:37 +0200

    increase maximum number of file descriptors
    see also https://qt.gitorious.org/qt-creator/qt-creator/commit/7f1f9e1

--- Renderer/NatronRenderer_main.cpp
+++ Renderer/NatronRenderer_main.cpp
@@ -15,6 +15,20 @@
 
 int main(int argc, char *argv[])
 {
+#if defined (Q_OS_MAC)
+    /*
+     Avoid 'Too many open files' on Mac
+
+     Increase the number of file descriptors that the process can open to the maximum allowed.
+     By default, Mac OS X only allows 256 file descriptors, which can easily be reached.
+     */
+    // see also https://qt.gitorious.org/qt-creator/qt-creator/commit/7f1f9e1
+    // increase maximum numbers of file descriptors
+	struct rlimit rl;
+	getrlimit(RLIMIT_NOFILE, &rl);
+ 	rl.rlim_cur = qMin((rlim_t)OPEN_MAX, rl.rlim_max);
+	setrlimit(RLIMIT_NOFILE, &rl);
+#endif
 
     bool isBackground;
     QString projectName,mainProcessServerName;
