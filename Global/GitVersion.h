5ae5735f4fdc8de5cca2684878b39fbdb7d00dca
MrKepzie <alexandre230@msn.com>
2014-06-20 22:52:50 +0200

    Flag for version 0.93: upload new binaries.

--- Global/GitVersion.h
+++ Global/GitVersion.h
@@ -1,5 +1,5 @@
 #ifndef NATRON_GITVERSION_H_
 #define NATRON_GITVERSION_H_
 #define GIT_BRANCH "workshop"
-#define GIT_COMMIT "add928a1cf4ab0b6bb2e80cc9c4eb8af22933228"
+#define GIT_COMMIT "723d54c0c5f6385b93baa9c310e76bca9a312d23"
 #endif
