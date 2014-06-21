be577aeaded9448b837fccbf7cb98c873fde4cbf
MrKepzie <alexandre230@msn.com>
2014-06-21 22:35:47 +0200

    Stylesheet: QWidget add the colour attribute.

--- Global/GitVersion.h
+++ Global/GitVersion.h
@@ -1,5 +1,5 @@
 #ifndef NATRON_GITVERSION_H_
 #define NATRON_GITVERSION_H_
 #define GIT_BRANCH "workshop"
-#define GIT_COMMIT "2394561b2ed7b3125f1b8890ab8124dc02084806"
+#define GIT_COMMIT "2eea6df6e100d2f0584e2bfbb04997f9e5d95f78"
 #endif
