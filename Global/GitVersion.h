8dba9206e9ba6a105791378b6e8cf60ff1463566
MrKepzie <alexandre230@msn.com>
2014-06-24 12:17:39 +0200

    Flag for 0.9.4

--- Global/GitVersion.h
+++ Global/GitVersion.h
@@ -1,5 +1,5 @@
 #ifndef NATRON_GITVERSION_H_
 #define NATRON_GITVERSION_H_
 #define GIT_BRANCH "workshop"
-#define GIT_COMMIT "228dfb15588cfc804dfeb33c7e82bbb6cb871da0"
+#define GIT_COMMIT "ec5099d080b17737c4c66157f82460388a970bac"
 #endif
