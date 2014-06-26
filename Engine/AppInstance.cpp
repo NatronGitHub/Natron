NatronGitHub/Natron

commit 3f9a7338e14179695077568c56adb65bc3ce98fa

The tracker sub label must be set by the UI
- the user may want to give a specific name to a track, e.g. « top
left »
- it has nothing to do with the node name
- it has to be saved in the project

Engine/AppInstance.cpp
@@ -178,7 +178,7 @@ void AppInstance::load(const QString& projectName,const QStringList& writers)
 }
 
 boost::shared_ptr<Natron::Node> AppInstance::createNodeInternal(const QString& pluginID,int majorVersion,int minorVersion,
-                                 bool requestedByLoad,bool openImageFileDialog,const NodeSerialization& serialization,bool dontLoadName)
+                                 bool requestedByLoad,bool openImageFileDialog,const NodeSerialization& serialization)
 {
     boost::shared_ptr<Node> node;
     LibraryBinary* pluginBinary = 0;
@@ -200,7 +200,7 @@ boost::shared_ptr<Natron::Node> AppInstance::createNodeInternal(const QString& p
     
     
     try{
-        node->load(pluginID.toStdString(),node, serialization,dontLoadName);
+        node->load(pluginID.toStdString(),node, serialization);
     } catch (const std::exception& e) {
         std::string title = std::string("Error while creating node");
         std::string message = title + " " + pluginID.toStdString() + ": " + e.what();
@@ -223,8 +223,8 @@ boost::shared_ptr<Natron::Node> AppInstance::createNodeInternal(const QString& p
 
 }
 
-boost::shared_ptr<Natron::Node> AppInstance::createNode(const QString& name,int majorVersion,int minorVersion,bool openImageFileDialog) {
-    
+boost::shared_ptr<Natron::Node> AppInstance::createNode(const QString& name,int majorVersion,int minorVersion,bool openImageFileDialog)
+{    
     ///use the same entry point to create backdrops.
     ///Since they are purely GUI we don't actually return a node.
     if (name == NATRON_BACKDROP_NODE_NAME) {
@@ -232,12 +232,12 @@ boost::shared_ptr<Natron::Node> AppInstance::createNode(const QString& name,int 
         return boost::shared_ptr<Natron::Node>();
     }
     return createNodeInternal(name, majorVersion, minorVersion, false,
-                              openImageFileDialog, NodeSerialization(boost::shared_ptr<Natron::Node>()),false);
+                              openImageFileDialog, NodeSerialization(boost::shared_ptr<Natron::Node>()));
 }
 
-boost::shared_ptr<Natron::Node> AppInstance::loadNode(const QString& name,int majorVersion,int minorVersion,const NodeSerialization& serialization,bool dontLoadName) {
-    return createNodeInternal(name, majorVersion, minorVersion, true, false, serialization,dontLoadName);
-
+boost::shared_ptr<Natron::Node> AppInstance::loadNode(const QString& name,int majorVersion,int minorVersion,const NodeSerialization& serialization)
+{
+    return createNodeInternal(name, majorVersion, minorVersion, true, false, serialization);
 }
 
 int AppInstance::getAppID() const { return _imp->_appID; }

Engine/AppInstance.h
@@ -77,7 +77,7 @@ public:
                                                bool openImageFileDialog = true);
     
     ///Same as createNode but used when loading a project
-    boost::shared_ptr<Natron::Node> loadNode(const QString& name,int majorVersion,int minorVersion,const NodeSerialization& serialization,bool dontLoadName);
+    boost::shared_ptr<Natron::Node> loadNode(const QString& name,int majorVersion,int minorVersion,const NodeSerialization& serialization);
 
     void getActiveNodes(std::vector<boost::shared_ptr<Natron::Node> > *activeNodes) const;
 
@@ -170,8 +170,7 @@ private:
     
     
     boost::shared_ptr<Natron::Node> createNodeInternal(const QString& pluginID,int majorVersion,int minorVersion,
-                                     bool requestedByLoad,bool openImageFileDialog,const NodeSerialization& serialization,
-                                     bool dontLoadName);
+                                     bool requestedByLoad,bool openImageFileDialog,const NodeSerialization& serialization);
     
     boost::scoped_ptr<AppInstancePrivate> _imp;
     

Engine/Node.cpp
@@ -203,19 +203,13 @@ void Node::createRotoContextConditionnally()
 }
 
 void Node::load(const std::string& pluginID,const boost::shared_ptr<Natron::Node>& thisShared,
-                const NodeSerialization& serialization,bool dontLoadName) {
-    
+                const NodeSerialization& serialization)
+{
     ///Called from the main thread. MT-safe
     assert(QThread::currentThread() == qApp->thread());
     
     ///cannot load twice
     assert(!_imp->liveInstance);
-    
-    bool nameSet = false;
-    if (!serialization.isNull() && !dontLoadName) {
-        setName(serialization.getPluginLabel().c_str());
-        nameSet = true;
-    }
     
     std::pair<bool,EffectBuilder> func = _imp->plugin->findFunction<EffectBuilder>("BuildEffect");
     if (func.first) {
@@ -237,24 +231,20 @@ void Node::load(const std::string& pluginID,const boost::shared_ptr<Natron::Node
     initializeInputs();
     initializeKnobs(serialization);
 
-    if (!nameSet) {
-         getApp()->getProject()->initNodeCountersAndSetName(this);
-    } else {
-        updateEffectLabelKnob(serialization.getPluginLabel().c_str());
-    }
-    
-    
+    getApp()->getProject()->initNodeCountersAndSetName(this);
+
     computeHash(); 
     assert(_imp->liveInstance);
 }
 
-U64 Node::getHashValue() const {
+U64 Node::getHashValue() const
+{
     QReadLocker l(&_imp->knobsAgeMutex);
     return _imp->hash.value();
 }
 
-void Node::computeHash() {
-    
+void Node::computeHash()
+{    
     ///Always called in the main thread
     assert(QThread::currentThread() == qApp->thread());
     
@@ -538,20 +528,7 @@ void Node::setName(const QString& name)
         QMutexLocker l(&_imp->nameMutex);
         _imp->name = name.toStdString();
     }
-    updateEffectLabelKnob(name);
     emit nameChanged(name);
-}
-
-void Node::updateEffectLabelKnob(const QString& name)
-{
-    if (!_imp->liveInstance) {
-        return;
-    }
-    boost::shared_ptr<KnobI> knob = getKnobByName(kOfxParamStringEffectInstanceLabel);
-    String_Knob* strKnob = dynamic_cast<String_Knob*>(knob.get());
-    if (strKnob) {
-        strKnob->setValue(name.toStdString(), 0);
-    }
 }
 
 AppInstance* Node::getApp() const
@@ -1830,7 +1807,7 @@ void Node::onEffectKnobValueChanged(KnobI* what,Natron::ValueChangedReason reaso
     } else if (what == _imp->nodeLabelKnob.get()) {
         emit nodeExtraLabelChanged(_imp->nodeLabelKnob->getValue().c_str());
     } else if (what->getName() == kOfxParamStringSublabelName) {
-        //special hack for the merge node so we can retrieve the operation as a string and display it in the node's label
+        //special hack for the merge node and others so we can retrieve the sublabel and display it in the node's label
         String_Knob* strKnob = dynamic_cast<String_Knob*>(what);
         if (what) {
             QString operation = strKnob->getValue().c_str();

Engine/Node.h
@@ -59,7 +59,7 @@ public:
     virtual ~Node();
     
     void load(const std::string& pluginID,const boost::shared_ptr<Natron::Node>& thisShared,
-              const NodeSerialization& serialization,bool dontLoadName);
+              const NodeSerialization& serialization/*,bool dontLoadName*/);
     
     ///called by load() and OfxEffectInstance, do not call this!
     void loadKnobs(const NodeSerialization& serialization);

Engine/ProjectPrivate.cpp
@@ -129,7 +129,7 @@ void ProjectPrivate::restoreFromSerialization(const ProjectSerialization& obj){
          ///this code may throw an exception which will be caught above
         boost::shared_ptr<Natron::Node> n = project->getApp()->loadNode(it->getPluginID().c_str()
                                                         ,it->getPluginMajorVersion()
-                                                        ,it->getPluginMinorVersion(),*it,false);
+                                                        ,it->getPluginMinorVersion(),*it);
         if (!n) {
             project->clearNodes();
             QString text("Failed to restore the graph! \n The node ");

commit e072dc802ae7f0ae4aafa70f1a477c57aafdd06b

Fix bug in the loading of projects that was introduced in https://github.com/MrKepzie/Natron/commit/3f9a7338e14179695077568c56adb6 d6c720913b

Engine/AppInstance.cpp
@@ -178,7 +178,7 @@ void AppInstance::load(const QString& projectName,const QStringList& writers)
 }
 
 boost::shared_ptr<Natron::Node> AppInstance::createNodeInternal(const QString& pluginID,int majorVersion,int minorVersion,
-                                 bool requestedByLoad,bool openImageFileDialog,const NodeSerialization& serialization)
+                                 bool requestedByLoad,bool openImageFileDialog,const NodeSerialization& serialization,bool dontLoadName)
 {
     boost::shared_ptr<Node> node;
     LibraryBinary* pluginBinary = 0;
@@ -200,7 +200,7 @@ boost::shared_ptr<Natron::Node> AppInstance::createNodeInternal(const QString& p
     
     
     try{
-        node->load(pluginID.toStdString(),node, serialization);
+        node->load(pluginID.toStdString(),node, serialization,dontLoadName);
     } catch (const std::exception& e) {
         std::string title = std::string("Error while creating node");
         std::string message = title + " " + pluginID.toStdString() + ": " + e.what();
@@ -232,12 +232,12 @@ boost::shared_ptr<Natron::Node> AppInstance::createNode(const QString& name,int 
         return boost::shared_ptr<Natron::Node>();
     }
     return createNodeInternal(name, majorVersion, minorVersion, false,
-                              openImageFileDialog, NodeSerialization(boost::shared_ptr<Natron::Node>()));
+                              openImageFileDialog, NodeSerialization(boost::shared_ptr<Natron::Node>()),false);
 }
 
-boost::shared_ptr<Natron::Node> AppInstance::loadNode(const QString& name,int majorVersion,int minorVersion,const NodeSerialization& serialization)
+boost::shared_ptr<Natron::Node> AppInstance::loadNode(const QString& name,int majorVersion,int minorVersion,const NodeSerialization& serialization,bool dontLoadName)
 {
-    return createNodeInternal(name, majorVersion, minorVersion, true, false, serialization);
+    return createNodeInternal(name, majorVersion, minorVersion, true, false, serialization,dontLoadName);
 }
 
 int AppInstance::getAppID() const { return _imp->_appID; }

Engine/AppInstance.h
@@ -77,7 +77,8 @@ public:
                                                bool openImageFileDialog = true);
     
     ///Same as createNode but used when loading a project
-    boost::shared_ptr<Natron::Node> loadNode(const QString& name,int majorVersion,int minorVersion,const NodeSerialization& serialization);
+    boost::shared_ptr<Natron::Node> loadNode(const QString& name,int majorVersion,int minorVersion,const NodeSerialization& serialization,
+                                             bool dontLoadName);
 
     void getActiveNodes(std::vector<boost::shared_ptr<Natron::Node> > *activeNodes) const;
 
@@ -170,7 +171,7 @@ private:
     
     
     boost::shared_ptr<Natron::Node> createNodeInternal(const QString& pluginID,int majorVersion,int minorVersion,
-                                     bool requestedByLoad,bool openImageFileDialog,const NodeSerialization& serialization);
+                                     bool requestedByLoad,bool openImageFileDialog,const NodeSerialization& serialization,bool dontLoadName);
     
     boost::scoped_ptr<AppInstancePrivate> _imp;
     

Engine/Node.cpp
@@ -203,13 +203,20 @@ void Node::createRotoContextConditionnally()
 }
 
 void Node::load(const std::string& pluginID,const boost::shared_ptr<Natron::Node>& thisShared,
-                const NodeSerialization& serialization)
+                const NodeSerialization& serialization,bool dontLoadName)
 {
     ///Called from the main thread. MT-safe
     assert(QThread::currentThread() == qApp->thread());
     
     ///cannot load twice
     assert(!_imp->liveInstance);
+    
+    bool nameSet = false;
+    if (!serialization.isNull() && !dontLoadName) {
+        setName(serialization.getPluginLabel().c_str());
+        nameSet = true;
+    }
+    
     
     std::pair<bool,EffectBuilder> func = _imp->plugin->findFunction<EffectBuilder>("BuildEffect");
     if (func.first) {
@@ -231,7 +238,9 @@ void Node::load(const std::string& pluginID,const boost::shared_ptr<Natron::Node
     initializeInputs();
     initializeKnobs(serialization);
 
-    getApp()->getProject()->initNodeCountersAndSetName(this);
+    if (!nameSet) {
+        getApp()->getProject()->initNodeCountersAndSetName(this);
+    }
 
     computeHash(); 
     assert(_imp->liveInstance);

Engine/Node.h
@@ -59,7 +59,7 @@ public:
     virtual ~Node();
     
     void load(const std::string& pluginID,const boost::shared_ptr<Natron::Node>& thisShared,
-              const NodeSerialization& serialization/*,bool dontLoadName*/);
+              const NodeSerialization& serialization,bool dontLoadName);
     
     ///called by load() and OfxEffectInstance, do not call this!
     void loadKnobs(const NodeSerialization& serialization);

Engine/ProjectPrivate.cpp
@@ -129,7 +129,7 @@ void ProjectPrivate::restoreFromSerialization(const ProjectSerialization& obj){
          ///this code may throw an exception which will be caught above
         boost::shared_ptr<Natron::Node> n = project->getApp()->loadNode(it->getPluginID().c_str()
                                                         ,it->getPluginMajorVersion()
-                                                        ,it->getPluginMinorVersion(),*it);
+                                                        ,it->getPluginMinorVersion(),*it,false);
         if (!n) {
             project->clearNodes();
             QString text("Failed to restore the graph! \n The node ");

Global/GitVersion.h
@@ -1,5 +1,5 @@
 #ifndef NATRON_GITVERSION_H_
 #define NATRON_GITVERSION_H_
 #define GIT_BRANCH "workshop"
-#define GIT_COMMIT "4a651b32d9a6ea27925b8ff46ba2e92318710faf"
+#define GIT_COMMIT "d9b848eea8bcc3aba8b140074024ed3ac75fbecb"
 #endif

Gui/NodeGraph.cpp
@@ -2217,7 +2217,7 @@ NodeGraph::pasteNode(const NodeSerialization& internalSerialization,
 {
     boost::shared_ptr<Natron::Node> n = _gui->getApp()->loadNode(internalSerialization.getPluginID().c_str(),
                                                internalSerialization.getPluginMajorVersion(),
-                                               internalSerialization.getPluginMinorVersion(),internalSerialization);
+                                               internalSerialization.getPluginMinorVersion(),internalSerialization,false);
     assert(n);
     const std::string& masterNodeName = internalSerialization.getMasterNodeName();
     if (masterNodeName.empty()) {
@@ -2325,7 +2325,7 @@ NodeGraph::cloneNode(const boost::shared_ptr<NodeGui>& node)
     
     boost::shared_ptr<Natron::Node> clone = _gui->getApp()->loadNode(internalSerialization.getPluginID().c_str(),
                                                internalSerialization.getPluginMajorVersion(),
-                                               internalSerialization.getPluginMinorVersion(),internalSerialization);
+                                               internalSerialization.getPluginMinorVersion(),internalSerialization,true);
     assert(clone);
     const std::string& masterNodeName = internalSerialization.getMasterNodeName();
     