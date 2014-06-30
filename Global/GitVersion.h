d44ca25b3bb2dbae7ee00b0e7dc164a2ceb4ba5f
MrKepzie <alexandre230@msn.com>
2014-06-30 17:44:55 +0200

    Fix old slot

--- Global/GitVersion.h
+++ Global/GitVersion.h
@@ -1,5 +1,5 @@
 #ifndef NATRON_GITVERSION_H_
 #define NATRON_GITVERSION_H_
 #define GIT_BRANCH "workshop"
-#define GIT_COMMIT "be932b644de87fdd2a7504e7ad599af99ee1d3ee"
+#define GIT_COMMIT "9b3030fee37d2ca54dfa8c85ca3f64f81b5daf56"
 #endif
