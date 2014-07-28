4f4bde01e06913b678ef7a819883ad950e8b7037
MrKepzie <alexandre230@msn.com>
2014-07-28 23:22:32 +0200

    Fixed a small stylesheet bug with combo boxes

--- Global/GitVersion.h
+++ Global/GitVersion.h
@@ -1,5 +1,5 @@
 #ifndef NATRON_GITVERSION_H_
 #define NATRON_GITVERSION_H_
 #define GIT_BRANCH "workshop"
-#define GIT_COMMIT "fe4791ef4a72b3a9f58f8bdf482b174f491f89c7"
+#define GIT_COMMIT "9dde20ad3d79d0dddbd0c36e08b99206fdd4c55c"
 #endif
