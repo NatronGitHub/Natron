9050e43734218c010d35ecbdee22eba3dd7a3434
MrKepzie <alexandre230@msn.com>
2014-08-01 00:33:49 +0200

    Remove tests that are no longer supported since the changes made in SequenceParsing

--- Global/GitVersion.h
+++ Global/GitVersion.h
@@ -1,5 +1,5 @@
 #ifndef NATRON_GITVERSION_H_
 #define NATRON_GITVERSION_H_
 #define GIT_BRANCH "workshop"
-#define GIT_COMMIT "8f72e9c02b32d7c61096b053554aec3ec63bcaaf"
+#define GIT_COMMIT "b302bcde236cfd81d617077c23636b8b6982b6a2"
 #endif
