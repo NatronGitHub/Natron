c5605b182e812be9aa1da8a48e3751b42328dea4
Frédéric Devernay <frederic.devernay@m4x.org>
2014-08-02 18:27:31 +0200

    update GitVersion

--- Global/GitVersion.h
+++ Global/GitVersion.h
@@ -1,5 +1,5 @@
 #ifndef NATRON_GITVERSION_H_
 #define NATRON_GITVERSION_H_
 #define GIT_BRANCH "workshop"
-#define GIT_COMMIT "3c0fac4aab6ffddf458887625468ecc7023d26d0"
+#define GIT_COMMIT "4335bffc9cc6e887ac9e902e957117f91e4244b0"
 #endif
