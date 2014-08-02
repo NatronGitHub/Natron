ebf143cbcc4260de2aab794289c984cc4f0f9d7c
Frédéric Devernay <frederic.devernay@m4x.org>
2014-08-02 17:41:50 +0200

    OfxEffectInstance: format

--- Global/GitVersion.h
+++ Global/GitVersion.h
@@ -1,5 +1,5 @@
 #ifndef NATRON_GITVERSION_H_
 #define NATRON_GITVERSION_H_
 #define GIT_BRANCH "workshop"
-#define GIT_COMMIT "10877319841bcea7c06e5671a1d9cd6aad065fdd"
+#define GIT_COMMIT "a5a19e94fc1d4288c28afcf5482e4db17a3b3982"
 #endif
