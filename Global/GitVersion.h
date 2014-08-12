bbfa0c34d9cc75e290ebc2b645e98de78033728b
MrKepzie <alexandre230@msn.com>
2014-08-11 22:21:22 -0700

    Also change .icns and .ico of Natron' logo

--- Global/GitVersion.h
+++ Global/GitVersion.h
@@ -1,5 +1,5 @@
 #ifndef NATRON_GITVERSION_H_
 #define NATRON_GITVERSION_H_
 #define GIT_BRANCH "workshop"
-#define GIT_COMMIT "d9526411b7719ffa26084a90a79dc51ee776ee97"
+#define GIT_COMMIT "501df1c707ec7913200fe7f9ddf11770cb921a9c"
 #endif
