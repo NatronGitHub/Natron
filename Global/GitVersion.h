e81706d706b3b9cdef13ddb4b7b60c4e593b88db
MrKepzie <alexandre230@msn.com>
2014-08-12 23:57:45 -0700

    Fix the user roi in ViewerGL

--- Global/GitVersion.h
+++ Global/GitVersion.h
@@ -1,5 +1,5 @@
 #ifndef NATRON_GITVERSION_H_
 #define NATRON_GITVERSION_H_
 #define GIT_BRANCH "workshop"
-#define GIT_COMMIT "c12d8e3847c48a560fe812046a028411b4573cf7"
+#define GIT_COMMIT "70ae3e3fcdb56fb443b1d654834040b085f13c2f"
 #endif
