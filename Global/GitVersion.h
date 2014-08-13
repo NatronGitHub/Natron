d87b84d43dea0ffa306f665f585490f2889e20c1
MrKepzie <alexandre230@msn.com>
2014-08-12 23:24:21 -0700

    Forgot to register the version of the format class to boost

--- Global/GitVersion.h
+++ Global/GitVersion.h
@@ -1,5 +1,5 @@
 #ifndef NATRON_GITVERSION_H_
 #define NATRON_GITVERSION_H_
 #define GIT_BRANCH "workshop"
-#define GIT_COMMIT "ab3b1a063d0d32c7b9d06027d31a5456b1f5db37"
+#define GIT_COMMIT "c12d8e3847c48a560fe812046a028411b4573cf7"
 #endif
