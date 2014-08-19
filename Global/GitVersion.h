c6e05877f563231d68333a6fe683105231219f6d
MrKepzie <alexandre230@msn.com>
2014-08-19 18:41:15 +0200

    Place the warning correctly

--- Global/GitVersion.h
+++ Global/GitVersion.h
@@ -1,5 +1,5 @@
 #ifndef NATRON_GITVERSION_H_
 #define NATRON_GITVERSION_H_
 #define GIT_BRANCH "workshop"
-#define GIT_COMMIT "11fd54d9c7e7e498b99227e9d20d9ea79b2af4a0"
+#define GIT_COMMIT "039a7f00334ae08053ea3bf3e32782970393ba29"
 #endif
