e4f513fff548e1005ab3bdad56f920c4f816d58f
Frédéric Devernay <frederic.devernay@m4x.org>
2014-08-19 18:19:17 +0200

    GuiMacros: add modifiers and buttons checking macros (for readability!)
    - note that for modifiers we test for *equality*, not bit-per-bit.

---
+++ Gui/GuiMacros.h
@@ -0,0 +1,26 @@
+//
+//  GuiMacros.h
+//  Gui
+//
+//  Created by Frédéric Devernay on 19/08/2014.
+//
+//
+
+#ifndef Gui_GuiMacros_h
+#define Gui_GuiMacros_h
+
+#define modifierIsNone(e)            ((e)->modifiers() == Qt::NoModifier)
+#define modifierIsControl(e)         ((e)->modifiers() == Qt::ControlModifier)
+#define modifierIsControlShift(e)    ((e)->modifiers() == (Qt::ControlModifier|Qt::ShiftModifier))
+#define modifierIsControlAlt(e)      ((e)->modifiers() == (Qt::ControlModifier|Qt::AltModifier))
+#define modifierIsControlAltShift(e) ((e)->modifiers() == (Qt::ControlModifier|Qt::AltModifier|Qt::ShiftModifier))
+#define modifierIsAlt(e)             ((e)->modifiers() == Qt::AltModifier)
+#define modifierIsAltShift(e)        ((e)->modifiers() == (Qt::AltModifier|Qt::ShiftModifier))
+#define modifierIsShift(e)           ((e)->modifiers() == Qt::ShiftModifier)
+
+#define buttonIsLeft(e)              ((e)->button() == Qt::LeftButton && modifierIsNone(e))
+#define buttonIsMiddle(e)            (((e)->button() == Qt::MiddleButton && modifierIsNone(e)) || (e->button() == Qt::LeftButton && modifierIsAlt(e)))
+#define buttonIsRight(e)             (((e)->button() == Qt::RightButton && modifierIsNone(e)) || (e->button() == Qt::LeftButton && modifierIsControl(e)))
+#define buttonIsRightShift(e)             (((e)->button() == Qt::RightButton && modifierIsShift(e)) || (e->button() == Qt::LeftButton && modifierIsControlShift(e)))
+
+#endif
