32aef25a42bbba9403d4eaa76e9d11216f2bfc7b
MrKepzie <alexandre230@msn.com>
2014-08-20 08:46:34 +0200

    Make combo box as tall as MEDIUM_BUTTONS

--- Global/GitVersion.h
+++ Global/GitVersion.h
@@ -1,5 +1,5 @@
 #ifndef NATRON_GITVERSION_H_
 #define NATRON_GITVERSION_H_
 #define GIT_BRANCH "workshop"
-#define GIT_COMMIT "d085a6de15ee122cb5d7e13aeadc74be8658cc7b"
+#define GIT_COMMIT "89022efbe614c9dad66da1d84157f621d48bece8"
 #endif
