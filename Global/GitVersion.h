12101a5b101677604be1b9645622557ac9042108
MrKepzie <alexandre230@msn.com>
2014-08-20 18:16:00 +0200

    Change icon

--- Global/GitVersion.h
+++ Global/GitVersion.h
@@ -1,5 +1,5 @@
 #ifndef NATRON_GITVERSION_H_
 #define NATRON_GITVERSION_H_
 #define GIT_BRANCH "workshop"
-#define GIT_COMMIT "834bd542aa6abef072a37c7b041d626efdf995d6"
+#define GIT_COMMIT "8d3ecf5ab9ab255d7b3a8eeb4d03dc731e203974"
 #endif
