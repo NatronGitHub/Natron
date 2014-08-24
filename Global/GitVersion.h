b8722949b1d6d88f506deb189fffef9ef0c50a6f
MrKepzie <alexandre230@msn.com>
2014-08-25 00:28:26 +0200

    Add a draft of the OpenFX plug-in programming guide.

--- Global/GitVersion.h
+++ Global/GitVersion.h
@@ -1,5 +1,5 @@
 #ifndef NATRON_GITVERSION_H_
 #define NATRON_GITVERSION_H_
 #define GIT_BRANCH "workshop"
-#define GIT_COMMIT "14fa31cbc1492db9e51149fcc13a9946d04ad4b1"
+#define GIT_COMMIT "54fc096d75fa8376c884b22ef40222c4e20ac830"
 #endif
