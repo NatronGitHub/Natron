d723459300a0c6daf3ad71ce5fd49d50eb4fd3e9
MrKepzie <alexandre230@msn.com>
2014-08-26 10:36:32 +0200

    Plugin Programming guide: Add a walkthrough of the Invert plug-in with code commentaries and what are the pitfalls

--- Global/GitVersion.h
+++ Global/GitVersion.h
@@ -1,5 +1,5 @@
 #ifndef NATRON_GITVERSION_H_
 #define NATRON_GITVERSION_H_
 #define GIT_BRANCH "workshop"
-#define GIT_COMMIT "a4154b3e90ca893c32bf124a02a8d16318160a8c"
+#define GIT_COMMIT "c12547eb0b57a81da6cb30b54e927310a02ec84b"
 #endif
