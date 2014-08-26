763135bb854009bf7edf9fcc1b8829ba90ea5b50
MrKepzie <alexandre230@msn.com>
2014-08-26 11:14:22 +0200

    Programming guide: fix typos + add page links and table of contents

--- Global/GitVersion.h
+++ Global/GitVersion.h
@@ -1,5 +1,5 @@
 #ifndef NATRON_GITVERSION_H_
 #define NATRON_GITVERSION_H_
 #define GIT_BRANCH "workshop"
-#define GIT_COMMIT "c12547eb0b57a81da6cb30b54e927310a02ec84b"
+#define GIT_COMMIT "ba4314ed5abdfa0276802742b86958b2f52bf39c"
 #endif
