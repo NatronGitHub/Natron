e35f4697ba0905732a973783c0ac303d367eeadf
MrKepzie <alexandre230@msn.com>
2014-09-16 19:37:41 +0200

    Small bug fix

--- Global/GitVersion.h
+++ Global/GitVersion.h
@@ -1,5 +1,5 @@
 #ifndef NATRON_GITVERSION_H_
 #define NATRON_GITVERSION_H_
 #define GIT_BRANCH "workshop"
-#define GIT_COMMIT "1114cae38ad7e3c2db7447692acb6ad828c43900"
+#define GIT_COMMIT "3cddde92ef3c317e9f7f196bf2b9d90bb12ae56a"
 #endif
