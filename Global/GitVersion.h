d1fcaa36c1e538155ff6221fa9c2d7aa45e74389
MrKepzie <alexandre230@msn.com>
2014-09-18 08:15:13 +0200

    LinkToDialog : don’t duplicate entries for all dimensions

--- Global/GitVersion.h
+++ Global/GitVersion.h
@@ -1,5 +1,5 @@
 #ifndef NATRON_GITVERSION_H_
 #define NATRON_GITVERSION_H_
 #define GIT_BRANCH "workshop"
-#define GIT_COMMIT "0ad4ac1d78f8bf1192b8fe686925c333b830039e"
+#define GIT_COMMIT "c99e1b323b98be940f6408c01648763b75c90c11"
 #endif
