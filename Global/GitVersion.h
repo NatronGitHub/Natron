7060de1abce5e7bf90d0d94961475fbc80a4b6a0
MrKepzie <alexandre230@msn.com>
2014-09-26 14:08:53 +0200

    Give a style to the QMenuBar

--- Global/GitVersion.h
+++ Global/GitVersion.h
@@ -1,5 +1,5 @@
 #ifndef NATRON_GITVERSION_H_
 #define NATRON_GITVERSION_H_
 #define GIT_BRANCH "workshop"
-#define GIT_COMMIT "f48c51d08c6c64d9280bd7bf0d7a3437f0e4af44"
+#define GIT_COMMIT "3600863be67666f5e287a9fd390240505fc6327e"
 #endif
