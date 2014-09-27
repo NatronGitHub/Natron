ee466756be670237c2b2fba4668d2537ddde9f32
MrKepzie <alexandre230@msn.com>
2014-09-27 22:52:42 +0200

    User BlendSetter to setup premult before drawing first texture of the viewer. It corrects partially the drawing of the checkerboard in wipe/over/under modes. Minus mode is OK

--- Global/GitVersion.h
+++ Global/GitVersion.h
@@ -1,5 +1,5 @@
 #ifndef NATRON_GITVERSION_H_
 #define NATRON_GITVERSION_H_
 #define GIT_BRANCH "workshop"
-#define GIT_COMMIT "932ee1143a70ec86255abdd16d31bcf6f7404dd0"
+#define GIT_COMMIT "69196d2c206ee644edb193d6a6a4c1df0f3a67fe"
 #endif
