8f2ba3528c9bb2ba32f22252f3d2779e21bffc14
MrKepzie <alexandre230@msn.com>
2014-09-28 17:14:22 +0200

    Don't propose to load the RENDER_SAVE autosave and don’t put a * to the project’s window title when saving for the RENDER_SAVE

--- Global/GitVersion.h
+++ Global/GitVersion.h
@@ -1,5 +1,5 @@
 #ifndef NATRON_GITVERSION_H_
 #define NATRON_GITVERSION_H_
 #define GIT_BRANCH "workshop"
-#define GIT_COMMIT "777324a8bfed71c7beda78a3a3b54775a3e1c040"
+#define GIT_COMMIT "0edc0df02a15c42cf8d6c3aab6942e069d127e98"
 #endif
