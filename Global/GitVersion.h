65dd4b5920d11bddb8ec47f6c7c7fd063be63d6d
MrKepzie <alexandre230@msn.com>
2014-09-30 00:12:21 +0200

    Bug fix in KnobSerialization

--- Global/GitVersion.h
+++ Global/GitVersion.h
@@ -1,5 +1,5 @@
 #ifndef NATRON_GITVERSION_H_
 #define NATRON_GITVERSION_H_
 #define GIT_BRANCH "workshop"
-#define GIT_COMMIT "8e96b2b2f15243a36dcfae74def3bf2629bc28e3"
+#define GIT_COMMIT "2e0409be29e5188a571178d1cfa0fda8cbb02620"
 #endif
