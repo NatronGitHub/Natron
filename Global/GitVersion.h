a9ad703a3a5f6ae313f79589ba53f6f7048faa58
MrKepzie <alexandre230@msn.com>
2014-09-29 09:12:29 +0200

    Resize close tab icon

--- Global/GitVersion.h
+++ Global/GitVersion.h
@@ -1,5 +1,5 @@
 #ifndef NATRON_GITVERSION_H_
 #define NATRON_GITVERSION_H_
 #define GIT_BRANCH "workshop"
-#define GIT_COMMIT "1dd04267f7b897dfcf5d3d4896573acb1671e798"
+#define GIT_COMMIT "03b373fdf60b35b07b5605bcc4dc03feb58663d9"
 #endif
