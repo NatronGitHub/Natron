d6be85f2ce081b0c2468e920e551d8d5f3fad81e
MrKepzie <alexandre230@msn.com>
2014-09-29 09:18:07 +0200

    Shrink down to 15px

--- Global/GitVersion.h
+++ Global/GitVersion.h
@@ -1,5 +1,5 @@
 #ifndef NATRON_GITVERSION_H_
 #define NATRON_GITVERSION_H_
 #define GIT_BRANCH "workshop"
-#define GIT_COMMIT "03b373fdf60b35b07b5605bcc4dc03feb58663d9"
+#define GIT_COMMIT "4a14cbfd71509f2d8afb1d58cde18065212eb76d"
 #endif
