b583df9a71b4edcedc6ac6b47cc7da82e28867dc
MrKepzie <alexandre230@msn.com>
2014-10-13 23:15:36 +0200

    File dialog: restore drag&drop functionality that was gone when switching filesystem model

--- Global/GitVersion.h
+++ Global/GitVersion.h
@@ -1,5 +1,5 @@
 #ifndef NATRON_GITVERSION_H_
 #define NATRON_GITVERSION_H_
 #define GIT_BRANCH "workshop"
-#define GIT_COMMIT "2cf5963eb70ca47d991b636b15cda6a3b877532e"
+#define GIT_COMMIT "54d528fdb1c737e00dece44404b11b3b3c36fde9"
 #endif
