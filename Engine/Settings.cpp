563faf7241bf5a0b689532bfc09894e05ad07602
Alexandre Gauthier <alexandre230@msn.com>
2014-10-24 15:49:15 +0200

    Add Blender OCIO config + default to it. This configs adds tons of looks that can be used with OCIO nodes.

--- Engine/Settings.cpp
+++ Engine/Settings.cpp
@@ -313,7 +313,7 @@ Settings::initializeKnobs()
         if ( ocioConfigsDir.exists() ) {
             QStringList entries = ocioConfigsDir.entryList(QDir::AllDirs | QDir::NoDotAndDotDot);
             for (int j = 0; j < entries.size(); ++j) {
-                if ( entries[j].contains("nuke-default") ) {
+                if ( entries[j].contains(NATRON_DEFAULT_OCIO_CONFIG_NAME) ) {
                     defaultIndex = j;
                 }
                 configs.push_back( entries[j].toStdString() );
