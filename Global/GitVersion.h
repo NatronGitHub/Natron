3314475804211dad5a902de7a0b062ef56331b83
MrKepzie <alexandre230@msn.com>
2014-11-19 23:46:39 +0100

    Don’t call checkClipPreferences recursively on project load

--- Global/GitVersion.h
+++ Global/GitVersion.h
@@ -1,5 +1,5 @@
 #ifndef NATRON_GITVERSION_H_
 #define NATRON_GITVERSION_H_
 #define GIT_BRANCH "workshop"
-#define GIT_COMMIT "26ea1b19d3bbd17de78873915b1242a36880be46"
+#define GIT_COMMIT "d4f29a19d06e1fd5a6a0557b994f34b2d42201d0"
 #endif
