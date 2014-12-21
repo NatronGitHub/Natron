93c0acdb5db246e4a9df48f7d1af834bdf5624d0
MrKepzie <alexandre230@msn.com>
2014-12-21 18:55:52 +0100

    Update bezier icon

--- Global/GitVersion.h
+++ Global/GitVersion.h
@@ -1,5 +1,5 @@
 #ifndef NATRON_GITVERSION_H_
 #define NATRON_GITVERSION_H_
 #define GIT_BRANCH "workshop"
-#define GIT_COMMIT "b7b276fa8f18f0f9ac360c5c50fbbfe4685e44bb"
+#define GIT_COMMIT "cff3073cfee7da62f3458aca635006f4591f4127"
 #endif
