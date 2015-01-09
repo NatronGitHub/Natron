57fb482e518f5abf47c762741482333f6469c687
Frédéric Devernay <frederic.devernay@m4x.org>
2015-01-09 11:47:08 +0100

    Travis: install python 3.4

--- tools/travis/install_dependencies.sh
+++ tools/travis/install_dependencies.sh
@@ -5,11 +5,7 @@ set -e
 # Print commands and their arguments as they are executed.
 set -x
 
-env
-python --version
-pip --version
-python3.4 --version
-pip3.4 --version
+PYTHON_VERSION=3.4
 
 # enable testing locally or on forks without multi-os enabled
 if [[ "${TRAVIS_OS_NAME:-false}" == false ]]; then
@@ -32,18 +28,27 @@ if [[ ${TRAVIS_OS_NAME} == "linux" ]]; then
     # the PPA xorg-edgers contains cairo 1.12 (required for rotoscoping)
     sudo add-apt-repository -y ppa:xorg-edgers/ppa 
     if [ "$CC" = "$TEST_CC" ]; then sudo pip install cpp-coveralls --use-mirrors; fi
+    # Python 3.4
+    sudo add-apt-repository --yes ppa:fkrull/deadsnakes # python3.x
     # we get libyaml-cpp-dev from kubuntu backports (for OpenColorIO)
     if [ "$CC" = "$TEST_CC" ]; then sudo add-apt-repository -y ppa:kubuntu-ppa/backports; fi
     sudo apt-get update
     sudo apt-get update -qq
 
-    sudo apt-get install libqt4-dev libglew-dev libboost-serialization-dev libexpat1-dev gdb libcairo2-dev
+    sudo apt-get install libqt4-dev libglew-dev libboost-serialization-dev libexpat1-dev gdb libcairo2-dev python3.4-dev
+
+    python --version
+    pip --version
+    python3 --version
+    pip3 --version
+    python3.4 --version
+    pip3.4 --version
 
     # PySide
     # see https://stackoverflow.com/questions/24489588/how-can-i-install-pyside-on-travis/24545890#24545890
-    pip${TRAVIS_PYTHON_VERSION} install PySide --no-index --find-links https://parkin.github.io/python-wheelhouse/;
+    pip${PYTHON_VERSION} install PySide --no-index --find-links https://parkin.github.io/python-wheelhouse/;
     # Travis CI servers use virtualenvs, so we need to finish the install by the following
-    python ~/virtualenv/python${TRAVIS_PYTHON_VERSION}/bin/pyside_postinstall.py -install
+    python ~/virtualenv/python${PYTHON_VERSION}/bin/pyside_postinstall.py -install
 
     # OpenFX
     if [ "$CC" = "$TEST_CC" ]; then make -C libs/OpenFX/Examples; fi
