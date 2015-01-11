27dd78fb466d5087cf98011c9a7e814beb787ea8
Frédéric Devernay <frederic.devernay@m4x.org>
2015-01-11 20:21:58 +0100

    Travis: try again

--- tools/travis/build.sh
+++ tools/travis/build.sh
@@ -30,7 +30,9 @@ git submodule update --init --recursive
 if [[ ${TRAVIS_OS_NAME} == "linux" ]]; then
     if [ "$CC" = "gcc" ]; then qmake -r CONFIG+="coverage debug"; else qmake -spec unsupported/linux-clang; fi
     make $J
+    if [ "$CC" = "gcc" ]; then cd Tests; ls ../Python; env OFX_PLUGIN_PATH=Plugins PYTHONPATH=../Python python3 -c "import natron"; cd ..; fi
     if [ "$CC" = "gcc" ]; then cd Tests; env OFX_PLUGIN_PATH=Plugins PYTHONPATH=../Python ./Tests; cd ..; fi
+    
 elif [[ ${TRAVIS_OS_NAME} == "osx" ]]; then
     # on OSX, the tests are done on the clang configuration
     # cairo requires xcb-shm, which has its pkg-config file in /opt/X11
