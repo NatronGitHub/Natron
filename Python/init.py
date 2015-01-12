5b3514acfea37d3259033e3630bbd1d99e16680b
Frédéric Devernay <frederic.devernay@m4x.org>
2015-01-12 09:13:46 +0100

    python: set coding in the first line

--- Python/init.py
+++ Python/init.py
@@ -1,5 +1,5 @@
-#Initialisation script for Natron
 # -*- coding: utf-8 -*-
+#Initialisation script for Natron
 #This Source Code Form is subject to the terms of the Mozilla Public
 #License, v. 2.0. If a copy of the MPL was not distributed with this
 #file, You can obtain one at http://mozilla.org/MPL/2.0/.
