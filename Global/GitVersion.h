3201cd7e4b9c17f1ef8ba965d9ca46d8f8ddbc55
MrKepzie <alexandre230@msn.com>
2015-01-13 23:25:49 +0100

    Fix unit tests compilation

--- Global/GitVersion.h
+++ Global/GitVersion.h
@@ -1,5 +1,5 @@
 #ifndef NATRON_GITVERSION_H_
 #define NATRON_GITVERSION_H_
 #define GIT_BRANCH "workshop"
-#define GIT_COMMIT "09a2f3d972e5042a76550000df9ddfd5db6a5a3b"
+#define GIT_COMMIT "176b62996abf7305d4f2d11ac68eca592d3bb152"
 #endif
