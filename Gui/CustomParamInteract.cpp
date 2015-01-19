93ff7c09128bd27f30fe05a1c2eb56c5c78e4b51
Frédéric Devernay <frederic.devernay@m4x.org>
2015-01-19 18:28:30 +0100

    fix

--- Gui/CustomParamInteract.cpp
+++ Gui/CustomParamInteract.cpp
@@ -59,7 +59,7 @@ CustomParamInteract::CustomParamInteract(KnobGui* knob,
     : QGLWidget(parent)
       , _imp( new CustomParamInteractPrivate(knob,ofxParamHandle,entryPoint) )
 {
-    int minW,minH;
+    double minW,minH;
 
     entryPoint->getMinimumSize(minW, minH);
     setMinimumSize(minW, minH);
