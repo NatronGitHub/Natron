150421d41822c6c71df786a3e5a59539241ab521
MrKepzie <alexandre230@msn.com>
2015-02-06 08:47:32 +0100

    Dimension name

--- Global/GitVersion.h
+++ Global/GitVersion.h
@@ -1,5 +1,5 @@
 #ifndef NATRON_GITVERSION_H_
 #define NATRON_GITVERSION_H_
 #define GIT_BRANCH "workshop"
-#define GIT_COMMIT "f410a0e26972349a47d89be5a6323b3e6fb53a42"
+#define GIT_COMMIT "89ed3731e92fbd73dfc667fa9c780ceca23d6eb1"
 #endif
