57689dd7f5b6771ee4e019ce4f1778f12b5692a8
MrKepzie <alexandre230@msn.com>
2015-02-09 23:45:57 +0100

    Update break pad

--- Global/GitVersion.h
+++ Global/GitVersion.h
@@ -1,5 +1,5 @@
 #ifndef NATRON_GITVERSION_H_
 #define NATRON_GITVERSION_H_
 #define GIT_BRANCH "workshop"
-#define GIT_COMMIT "2bed582b8f01d0622dff8794bcc4b839aecedb83"
+#define GIT_COMMIT "20f3f1e2c318d24f2e54dfd20cf78ba44a371f42"
 #endif
