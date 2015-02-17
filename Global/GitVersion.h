832ea9e28722417bf24d8dab9334f1026bdea955
MrKepzie <alexandre230@msn.com>
2015-02-17 23:55:21 +0100

    Tracker 2 bug fix: Don’t propose the multi-instance container as potential track link + repair track buttons from the settings panel. Fix #470

--- Global/GitVersion.h
+++ Global/GitVersion.h
@@ -1,5 +1,5 @@
 #ifndef NATRON_GITVERSION_H_
 #define NATRON_GITVERSION_H_
 #define GIT_BRANCH "workshop"
-#define GIT_COMMIT "5b58cf3989174130144d965e25c3b63863a07eca"
+#define GIT_COMMIT "91e44e92f961f9a711e11e46f6dfedbb6d2a88ae"
 #endif
