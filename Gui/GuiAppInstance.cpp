9db6bca8f711bde87d9eb3786fda6e9142d3563e
Frédéric Devernay <frederic.devernay@m4x.org>
2015-03-09 17:03:31 +0100

    GuiAppInstance: don't compute preview when loading

--- Gui/GuiAppInstance.cpp
+++ Gui/GuiAppInstance.cpp
@@ -361,12 +361,11 @@ GuiAppInstance::createNodeGui(const boost::shared_ptr<Natron::Node> &node,
 
     ///must be called after initializeKnobs as it populates the node's knobs in the curve editor;
     _imp->_gui->addNodeGuiToCurveEditor(nodegui);
-    
-    ///we make sure we can have a clean preview.
-    node->computePreviewImage( getTimeLine()->currentFrame() );
-    
-    
+
     if (!loadRequest && !isViewer) {
+        ///we make sure we can have a clean preview.
+        node->computePreviewImage( getTimeLine()->currentFrame() );
+        
         triggerAutoSave();
     }
 } // createNodeGui
