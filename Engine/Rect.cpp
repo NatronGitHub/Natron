ae3acfc8e459963862fb4ca42a2f224e2dc6769c
Frédéric Devernay <frederic.devernay@m4x.org>
2015-05-13 09:32:21 +0200

    splitIntoSmallerRects: fix when splitsCount == 0

--- Engine/Rect.cpp
+++ Engine/Rect.cpp
@@ -40,16 +40,23 @@ std::vector<RectI> RectI::splitIntoSmallerRects(int splitsCount) const
         }
     }
 #else
-    // make sure there are at least 4096 pixels (=64*64) per rect, and the rects are as square as possible.
+    // make sure there are at least MINAREA pixels (=128*128) per rect, and the rects are as square as possible.
     // This minimizes the overlapping areas between rendered regions
     if (area() <= MINAREA) {
         ret.push_back(*this);
     } else {
         // the average rect area
         double avgArea = splitsCount ? std::max((double)MINAREA, area() / (double)splitsCount) : MINAREA;
+        splitsCount = area() / avgArea;
         int numCols = std::max(1, (int)(width() / std::sqrt(avgArea)));
-        int numRows = std::max(1, std::min(splitsCount / numCols, (int)(area()/(MINAREA*numCols)))); // integer division
-        numCols = std::max(1, std::min(splitsCount / numRows, (int)(area()/(MINAREA*numRows))));
+        int numRows = std::max(1, (int)(area()/(MINAREA*numCols))); // integer division
+        if (splitsCount && splitsCount > numCols) {
+            numRows = std::min(splitsCount / numCols, numRows);
+        }
+        numCols = std::max(1, (int)(area()/(MINAREA*numRows)));
+        if (splitsCount && splitsCount > numRows) {
+            numCols = std::min(splitsCount / numRows, numCols);
+        }
         assert((splitsCount >= numRows * numCols) || !splitsCount);
         for (int i = 0; i < numRows; ++i) {
             int y1 = bottom() + i     * height() / numRows;
@@ -57,7 +64,7 @@ std::vector<RectI> RectI::splitIntoSmallerRects(int splitsCount) const
             for (int j = 0; j < numCols; ++j) {
                 int x1 = left() + j     * width() / numCols;
                 int x2 = left() + (j+1) * width() / numCols;
-                //assert((x2-x1)*(y2-y1) >= MINAREA);
+                assert((x2-x1)*(y2-y1) >= MINAREA);
                 //printf("area is %d\n", (x2-x1)*(y2-y1));
                 ret.push_back( RectI(x1, y1, x2, y2) );
             }
