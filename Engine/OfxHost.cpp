c933c412c2ad5a73df985797253a233299b2bf48
Frédéric Devernay <frederic.devernay@m4x.org>
2015-05-20 11:57:40 +0200

    update openfx (adds support for cascading choice menus)

--- Engine/OfxHost.cpp
+++ Engine/OfxHost.cpp
@@ -193,6 +193,7 @@ Natron::OfxHost::setProperties()
     ///Natron extensions
     _properties.setIntProperty(kNatronOfxHostIsNatron, 1);
     _properties.setIntProperty(kNatronOfxParamHostPropSupportsDynamicChoices, 1);
+    //_properties.setIntProperty(kNatronOfxParamPropChoiceCascading, 1);
 #endif
     
 }
