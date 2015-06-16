5143016a836b5b1533ce1643d7b081f2308c0340
Frédéric Devernay <frederic.devernay@m4x.org>
2015-06-16 12:34:09 +0200

    oops

--- Gui/ViewerGL.cpp
+++ Gui/ViewerGL.cpp
@@ -5251,11 +5251,11 @@ ViewerGL::currentTimeForEvent(QInputEvent* e)
     }
 #else
     (void)e;
+#endif
     // Qt 4 has no event timestamp, use gettimeofday (defined in Timer.cpp for windows)
     struct timeval now;
     gettimeofday(&now, 0);
     return now.tv_sec + now.tv_usec / 1000000.0;
-#endif
 }
 
 
