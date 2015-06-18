ae52625f9c3ee74ed8c73724ae09dc30116fa32b
Frédéric Devernay <frederic.devernay@m4x.org>
2015-06-18 23:28:25 +0200

    fix warnings

--- Engine/OfxEffectInstance.cpp
+++ Engine/OfxEffectInstance.cpp
@@ -27,7 +27,13 @@
 
 #include "Global/Macros.h"
 
+#include "Global/Macros.h"
+// ofxhPropertySuite.h:565:37: warning: 'this' pointer cannot be null in well-defined C++ code; comparison may be assumed to always evaluate to true [-Wtautological-undefined-compare]
+CLANG_DIAG_OFF(unknown-pragmas)
+CLANG_DIAG_OFF(tautological-undefined-compare) // appeared in clang 3.5
 #include <ofxhPluginCache.h>
+CLANG_DIAG_ON(tautological-undefined-compare)
+CLANG_DIAG_ON(unknown-pragmas)
 #include <ofxhPluginAPICache.h>
 #include <ofxhImageEffectAPI.h>
 #include <ofxhHost.h>
--- Gui/Gui.cpp
+++ Gui/Gui.cpp
@@ -56,10 +56,10 @@ CLANG_DIAG_ON(unused-private-field)
 #include <cairo/cairo.h>
 
 #include <boost/version.hpp>
-CLANG_DIAG_OFF(unused-parameter)
+GCC_DIAG_OFF(unused-parameter)
 // /opt/local/include/boost/serialization/smart_cast.hpp:254:25: warning: unused parameter 'u' [-Wunused-parameter]
 #include <boost/archive/xml_iarchive.hpp>
-CLANG_DIAG_ON(unused-parameter)
+GCC_DIAG_ON(unused-parameter)
 #include <boost/archive/xml_oarchive.hpp>
 
 #include "Engine/ViewerInstance.h"
