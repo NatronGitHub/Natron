e65140aa37b0e479d88e97702d4443aabbf2a664
AxelAF <axel.airofarulla@gmail.com>
2015-06-29 19:31:10 +0200

    Dope sheet : add some code documentation for the DopeSheet/DSNode/ DSKnob/DopeSheetKey classes

--- Gui/DopeSheet.h
+++ Gui/DopeSheet.h
@@ -59,9 +59,26 @@ bool nodeHasAnimation(const boost::shared_ptr<NodeGui> &nodeGui);
 
 
 /**
- * @brief The DopeSheet class
+ * @brief The DopeSheet class is the model part of the dope sheet editor.
+ *
+ * A DopeSheet is basically a bundle of "contexts". A context is a instance
+ * of a class composed with a QTreeWidgetItem and a Natron data. Currently,
+ * the class handles two types of contexts :
+ *
+ * - Node context : described by the DSNode class.
+ * - Knob context : described by the DSKnob class.
+ *
+ * It provides several functions to find contexts and some of their datas.
  *
  *
+ * --- User interactions ---
+ *
+ * The model handles each type of user interaction (that modify the model,
+ * of course). For example, a specific function move the selected keyframes,
+ * another move a reader...
+ *
+ * If you want to create a custom undoable modification, you must add a public
+ * function that push a QUndoCommand-derived instance on the model's undo stack.
  */
 class DopeSheet: public QObject
 {
@@ -153,9 +170,52 @@ private:
 
 
 /**
- * @brief The DSNode class
+ * @brief The DSNode class describes a node context in the dope sheet.
+ *
+ * A node context is basically composed of a QTreeWidgetItem and a
+ * NodeGui object.
+ *
+ * Use getTreeItem() to retrieve the tree widget item.
  *
  *
+ * --- Retrieve node data ---
+ *
+ * A node context store a NodeGui object, which can be retrieved by using
+ * getNodeGui().
+ *
+ * Use getInternalNode() to access to the Node object itself.
+ *
+ *
+ * --- Child contexts ---
+ *
+ * According to the structure of a node, a DSNode stores a map composed of :
+ * - a tree widget item named with a knob context text (knob name, dimension name).
+ * - a DSKnob object (see below).
+ *
+ * Use getItemKnobMap() to get this map.
+ *
+ * The DSNode/DSKnob API is the dopesheet version of the Node/Knob organization.
+ *
+ *
+ * -- Item types --
+ *
+ * Remember, the DopeSheet::ItemType enum describes each possible type for a
+ * context.
+ *
+ * A DSNode stores this type because the dope sheet editor treats some types
+ * of node differently. A DSNode can be a node exclusively composed of the
+ * keyframes of its knobs. Another can be a reader node, which must be
+ * drawn in a different way in the editor.
+ *
+ * Use getItemType() to retrieve this context type. Use isTimeNode() and
+ * isRangeDrawingEnabled() to have more detailed infos about it.
+ *
+ * If you created a new node context type, please modify
+ * isRangeDrawingEnabled() and canContainOtherNodeContexts() to handle it.
+ *
+ *
+ * /!\ A better conception should remove the QTreeWidgetItem attribute
+ * from this class.
  */
 class DSNode
 {
@@ -188,9 +248,62 @@ private:
 
 
 /**
- * @brief The DSKnob class
+ * @brief The DSKnob class describes a knob context in the dope sheet.
+ *
+ * A knob context is basically composed of a QTreeWidgetItem and a
+ * KnobGui object.
  *
  *
+ * --- Simple and multidimensional knobs ---
+ *
+ * How the dope sheet treats the knobs of a node ?
+ *
+ * The main rule is : one knob context per dimension.
+ *
+ * According to Natron, a knob can have more than one dimension. In this case,
+ * a DSKnob is created for each dimension and for the knob name.
+ *
+ * Example :
+ * A knob named "Color" has three dimensions : r, g, b. The dope sheet will
+ * treat it as :
+ *
+ * Color
+ * - r
+ * - g
+ * - b
+ *
+ * So four contexts.
+ *
+ *
+ * --- Dealing with dimensions ---
+ *
+ * A knob context also stores the dimension of the associated knob :
+ * - 0 if the knob has one dimension.
+ * - (N - X) if the knob has N dimensions. X refers to the current dimension.
+ * - (-1) if the knob context is the root context of a multidim knob.
+ *
+ * getDimension() returns the dimension of the context.
+ * isMuldiDimRoot() is a convenience function to check if the context is the
+ * root of a multidim knob.
+ *
+ *
+ * --- The QTreeWidgetItem-related API  ---
+ *
+ * Use getTreeItem() to retrieve the tree widget item.
+ *
+ * If 'this' is a knob root context and you want to obtain a pointer on the
+ * tree widget item associated with a X dimension, use findDimTreeItem().
+ *
+ *
+ * --- Retrieve knob data ---
+ *
+ * A knob context store a Knob object and the associated KnobGui object.
+ *
+ * Use getKnobGui() and getInternalKnob() to access to them.
+ *
+ *
+ * /!\ A better conception should remove the QTreeWidgetItem attribute
+ * from this class.
  */
 class DSKnob
 {
@@ -214,9 +327,9 @@ private:
 };
 
 /**
- * @brief The DSSelectedKey struct
+ * @brief The DopeSheetKey class describes a keyframe in a knob context.
  *
- *
+ * It is and should be used only to handle keyframe selections.
  */
 struct DopeSheetKey
 {
@@ -269,6 +382,12 @@ struct DopeSheetKey
 typedef boost::shared_ptr<DopeSheetKey> DSKeyPtr;
 typedef std::list<DSKeyPtr> DSKeyPtrList;
 
+
+/**
+ * @brief The DopeSheetSelectionModel class
+ *
+ *
+ */
 class DopeSheetSelectionModel : public QObject
 {
     Q_OBJECT
