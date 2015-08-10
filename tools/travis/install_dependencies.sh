04d822056d434b11e65b7e335988408100463bfc
Frédéric Devernay <frederic.devernay@m4x.org>
2015-08-11 01:29:35 +0200

    Travis: use GCC 4.9

--- tools/travis/install_dependencies.sh
+++ tools/travis/install_dependencies.sh
@@ -24,7 +24,10 @@ if [[ ${TRAVIS_OS_NAME} == "linux" ]]; then
     # we use the irie/boost ppa for that purpose
     sudo add-apt-repository -y ppa:irie/boost
     # the PPA xorg-edgers contains cairo 1.12 (required for rotoscoping)
-    sudo add-apt-repository -y ppa:xorg-edgers/ppa 
+    sudo add-apt-repository -y ppa:xorg-edgers/ppa
+    # ubuntu-toolchain-r/test contains recent versions of gcc
+    if [ "$CC" = "$TEST_CC" ]; then sudo add-apt-repository -y ppa:ubuntu-toolchain-r-test; sudo apt-get install gcc-4.9 g++-4.9; fi
+
     if [ "$CC" = "$TEST_CC" ]; then sudo pip install cpp-coveralls --use-mirrors; fi
     # Python 3.4
     #sudo add-apt-repository --yes ppa:fkrull/deadsnakes # python3.x
