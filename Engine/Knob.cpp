a3118f507a19fe21b98b4197d887097b4d82c5e6
Frédéric Devernay <frederic.devernay@m4x.org>
2015-09-07 09:56:37 +0200

    Knob: bug fix

--- Engine/Knob.cpp
+++ Engine/Knob.cpp
@@ -1592,9 +1592,10 @@ static bool parseTokenFrom(int fromDim,
     //The get() function does not always returns a tuple
     if (params.empty() && dimensionParamPos == -1) {
         params.push_back("-1");
+        ++dimensionParamPos;
     }
     
-    if (dimensionParamPos >= (int)params.size()) {
+    if (dimensionParamPos < 0 || (int)params.size() <= dimensionParamPos) {
         throw std::invalid_argument("Invalid expr");
     }
     
