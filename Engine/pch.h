9a0fdb3c7d2a3656666fcb8e1530b97a340d663d
Alexandre Gauthier <alexandre230@msn.com>
2015-09-07 10:12:39 +0200

    Fix pch

--- Engine/pch.h
+++ Engine/pch.h
@@ -1,7 +1,6 @@
 // -*-c++-*-
 #ifndef ENGINE_PCH_H
 #define ENGINE_PCH_H
-<
 // ***** BEGIN PYTHON BLOCK *****
 // from <https://docs.python.org/3/c-api/intro.html#include-files>:
 // "Since Python may define some pre-processor definitions which affect the standard headers on some systems, you must include Python.h before any standard headers are included."
