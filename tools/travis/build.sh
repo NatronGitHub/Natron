99fbd5a15b34fa8d9e1f577786fccfeb4ba727d4
Frédéric Devernay <frederic.devernay@m4x.org>
2015-09-11 19:32:35 +0200

    Travis: build with breakpad

--- tools/travis/build.sh
+++ tools/travis/build.sh
@@ -45,7 +45,7 @@ CMAKE_PREFIX_PATH=$(echo /usr/local/Cellar/*/* | sed 's/ /;/g')
 git submodule update --init --recursive
 
 if [[ ${TRAVIS_OS_NAME} == "linux" ]]; then
-    if [ "$CC" = "gcc" ]; then qmake -r CONFIG+="coverage debug"; else qmake -r -spec unsupported/linux-clang CONFIG+="debug"; fi
+    if [ "$CC" = "gcc" ]; then qmake -r CONFIG+="coverage debug breakpad"; else qmake -r -spec unsupported/linux-clang CONFIG+="debug breakpad"; fi
     # don't build parallel on the coverity_scan branch, because we reach the 3GB memory limit
     if [[ ${COVERITY_SCAN_BRANCH} == 1 ]]; then
 	# compiling Natron overrides the 3GB limit on travis if building parallel
@@ -60,7 +60,7 @@ elif [[ ${TRAVIS_OS_NAME} == "osx" ]]; then
     # on OSX, the tests are done on the clang configuration
     # cairo requires xcb-shm, which has its pkg-config file in /opt/X11
     export PKG_CONFIG_PATH=/opt/X11/lib/pkgconfig
-    if [ "$CC" = "gcc" ]; then qmake -r -spec unsupported/macx-clang-libc++ QMAKE_CC=gcc QMAKE_CXX=g++ CONFIG+="debug"; else qmake -spec unsupported/macx-clang-libc++ CONFIG+="debug"; fi
+    if [ "$CC" = "gcc" ]; then qmake -r -spec unsupported/macx-clang-libc++ QMAKE_CC=gcc QMAKE_CXX=g++ CONFIG+="debug breakpad"; else qmake -spec unsupported/macx-clang-libc++ CONFIG+="debug breakpad"; fi
     export MAKEFLAGS="$J" # qmake doesn't seem to pass MAKEFLAGS for recursive builds
     make $J
     if [ "$CC" = "clang" ]; then cd Tests; env OFX_PLUGIN_PATH=Plugins ./Tests; cd ..; fi
