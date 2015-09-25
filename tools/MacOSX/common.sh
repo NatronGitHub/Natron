b7e093a78ee9008c9e6a1431aa2e192fbe68092b
Alexandre Gauthier <alexandre230@msn.com>
2015-09-25 17:53:04 +0200

    Update git tags in common.sh

--- tools/MacOSX/common.sh
+++ tools/MacOSX/common.sh
@@ -4,11 +4,11 @@ CWD=`pwd`
 
 #THE FOLLOWING CAN BE MODIFIED TO CONFIGURE RELEASE BUILDS
 #----------------------------------------------------------
-NATRON_GIT_TAG=tags/2.0.0
-IOPLUG_GIT_TAG=tags/2.0.0
-MISCPLUG_GIT_TAG=tags/2.0.0
-ARENAPLUG_GIT_TAG=tags/2.0.0
-CVPLUG_GIT_TAG=tags/2.0.0
+NATRON_GIT_TAG=tags/2.0.0-RC1
+IOPLUG_GIT_TAG=tags/2.0.0-RC1
+MISCPLUG_GIT_TAG=tags/2.0.0-RC1
+ARENAPLUG_GIT_TAG=tags/2.0.0-RC1
+CVPLUG_GIT_TAG=tags/2.0.0-RC1
 #----------------------------------------------------------
 
 GIT_NATRON=https://github.com/MrKepzie/Natron.git
