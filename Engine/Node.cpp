af38051b700638e2b8b13ab7c0410e53022c8678
Alexandre Gauthier <alexandre230@msn.com>
2015-11-06 12:34:35 +0100

    move RGBA checkboxes on top of the panel

--- Engine/Node.cpp
+++ Engine/Node.cpp
@@ -2676,11 +2676,12 @@ Node::initializeKnobs(int renderScaleSupportPref)
                 }
                 
                 if (foundEnabled[0] && foundEnabled[1] && foundEnabled[2] && foundEnabled[3]) {
+                    
                     for (int i = 0; i < 4; ++i) {
                         if (foundEnabled[i]->getParentKnob() == mainPage) {
-                            foundEnabled[i]->setAddNewLine(i == 3);
+                            //foundEnabled[i]->setAddNewLine(i == 3);
                             mainPage->removeKnob(foundEnabled[i].get());
-                            mainPage->addKnob(foundEnabled[i]);
+                            mainPage->insertKnob(i,foundEnabled[i]);
                         }
                         _imp->enabledChan[i] = foundEnabled[i];
                     }
@@ -2698,7 +2699,7 @@ Node::initializeKnobs(int renderScaleSupportPref)
                         foundEnabled[i]->setAddNewLine(i == 3);
                         foundEnabled[i]->setDefaultValue(pluginDefaultPref[i]);
                         foundEnabled[i]->setHintToolTip(channelHints[i]);
-                        mainPage->addKnob(foundEnabled[i]);
+                        mainPage->insertKnob(i,foundEnabled[i]);
                         _imp->enabledChan[i] = foundEnabled[i];
                     }
                 }
