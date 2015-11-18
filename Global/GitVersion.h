8d5ea8c9234a96def2a3062fbb2bab965b10d7e4
MrKepzie <alexandre230@msn.com>
2015-11-18 23:25:39 +0100

    Fix include

--- Global/GitVersion.h
+++ Global/GitVersion.h
@@ -1,5 +1,5 @@
 #ifndef NATRON_GITVERSION_H_
 #define NATRON_GITVERSION_H_
 #define GIT_BRANCH "workshop"
-#define GIT_COMMIT "b5ab3731a143e78750c4a7503ea13006ed3a15cd"
+#define GIT_COMMIT "1f3d6cc37991806edea42c1050af52bb8ee4203f"
 #endif
