605b2132c301a070e91ece9468dc6ef41e8aee74
Frédéric Devernay <frederic.devernay@m4x.org>
2015-11-27 09:49:22 +0100

    use the STL force

--- Gui/ToolButton.cpp
+++ Gui/ToolButton.cpp
@@ -145,13 +145,12 @@ void
 ToolButton::tryAddChild(ToolButton* child)
 {
     assert(_imp->_menu);
-    for (unsigned int i = 0; i < _imp->_children.size(); ++i) {
-        if (_imp->_children[i] == child) {
-            return;
-        }
+    // insert if not present
+    std::vector<ToolButton*> &children = _imp->_children;
+    if (std::find(children.begin(), children.end(), child) == children.end()) {
+        children.push_back(child);
+        _imp->_menu->addAction( child->getAction() );
     }
-    _imp->_children.push_back(child);
-    _imp->_menu->addAction( child->getAction() );
 }
 
 const std::vector<ToolButton*> &
