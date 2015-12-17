b5d5c9bd84777c6104b724190f23bf48dd64c7ac
Ole-André Rodlie <olear@dracolinux.org>
2015-12-17 10:41:55 +0100

    socorro: add missing values

--- tools/socorro/make_repo_file.sh
+++ tools/socorro/make_repo_file.sh
@@ -7,8 +7,7 @@ REPORTS=/home/socorro/reports
 cd $REPORTS || exit 1
 echo > repo.txt
 for i in *.json; do
-  FDATE=`date -r ${i} +%F`
   MD5=`md5sum ${i}|awk '{print $1}'`
-  echo "$FDATE $MD5 $i" >> repo.txt
+  echo "$MD5 $i" >> repo.txt
 done
 
