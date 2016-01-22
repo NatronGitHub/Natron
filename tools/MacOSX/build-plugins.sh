bc2f074f6a91b0b8816736625a2af2e4024ca509
Alexandre Gauthier <alexandre230@msn.com>
2016-01-22 11:33:30 +0100

    OSX: fix build plugins symbols export

--- tools/MacOSX/build-plugins.sh
+++ tools/MacOSX/build-plugins.sh
@@ -122,15 +122,15 @@ cd ..
 #Dump symbols for breakpad before stripping
 if [ "$DISABLE_BREAKPAD" != "1" ]; then
     for bin in IO Misc CImg Arena; do
-        binary="$PLUGINDIR"/${bin}.ofx.bundle/*/*/$bin.ofx
-	
+        binary="$PLUGINDIR"/${bin}.ofx.bundle/Contents/MacOS/${bin}.ofx
+
 #       DSYM_64=${bin}x86_64.dSYM
 #		DSYM_32=${bin}i386.dSYM
 #        dsymutil -arch x86_64 -o $DSYM_64 "$binary"
 #        dsymutil -arch i386 -o $DSYM_32 "$binary"
 
-		$DUMP_SYMS -a x86_64 "$PLUGINDIR"/${bin}.ofx.bundle/Contents/$OS-$BITS-$CONFIG/${bin}.ofx  > "$CWD/build/symbols/${bin}.ofx-${TAG}-Mac-x86_64.sym"
-		$DUMP_SYMS -a i386 "$PLUGINDIR"/${bin}.ofx.bundle/Contents/$OS-$BITS-$CONFIG/${bin}.ofx  > "$CWD/build/symbols/${bin}.ofx-${TAG}-Mac-i386.sym"
+		$DUMP_SYMS -a x86_64 "$PLUGINDIR"/${bin}.ofx.bundle/Contents/MacOS/${bin}.ofx  > "$CWD/build/symbols/${bin}.ofx-${TAG}-Mac-x86_64.sym"
+		$DUMP_SYMS -a i386 "$PLUGINDIR"/${bin}.ofx.bundle/Contents/MacOS/${bin}.ofx  > "$CWD/build/symbols/${bin}.ofx-${TAG}-Mac-i386.sym"
 		
 #		rm -rf $DSYM_64
 #		rm -rf $DSYM_32
