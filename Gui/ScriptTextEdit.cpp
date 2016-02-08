8844c00c958303b9d7e7bf28d5a149dd6071f4d7
Alexandre Gauthier <alexandre230@msn.com>
2016-02-08 13:04:58 +0100

    fix includes

--- Gui/ScriptTextEdit.cpp
+++ Gui/ScriptTextEdit.cpp
@@ -43,6 +43,7 @@ CLANG_DIAG_ON(uninitialized)
 
 #include "Gui/Gui.h"
 #include "Gui/GuiAppInstance.h"
+#include "Gui/Gui.h"
 #include "Gui/GuiApplicationManager.h"
 #include "Gui/KnobWidgetDnD.h"
 
