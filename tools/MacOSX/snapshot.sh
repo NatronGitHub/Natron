bf5aacd71ca4518da7d942a7e407249726c113a5
Ole-André Rodlie <ole-andre.rodlie@inria.fr>
2016-04-05 16:10:27 +0200

    Mac: adapt to lin/win script changes

--- tools/MacOSX/snapshot.sh
+++ tools/MacOSX/snapshot.sh
@@ -1,172 +1,192 @@
 #!/bin/sh
-# ***** BEGIN LICENSE BLOCK *****
-# This file is part of Natron <http://www.natron.fr/>,
-# Copyright (C) 2016 INRIA and Alexandre Gauthier
-#
-# Natron is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-#
-# Natron is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with Natron.  If not, see <http://www.gnu.org/licenses/gpl-2.0.html>
-# ***** END LICENSE BLOCK *****
 
-#Usage:
-# cd Natron/tools/MacOSX
-# MKJOBS=4 ./snapshot.sh
+source `pwd`/common.sh
 
-source `pwd`/common.sh || exit 1
-
-
-if [ ! -d $TMP ];then
-    mkdir -p $TMP || exit 1
+if [ ! -d $TMP ]; then
+  mkdir -p $TMP || exit 1
+fi
+if [ ! -d $TMP/Natron ]; then
+  cd $TMP || exit 1
+  git clone $GIT_NATRON || exit 1
+#  cd Natron || exit 1
+#  git checkout $MASTER_BRANCH || exit 1
+fi
+if [ ! -d $TMP/openfx-io ]; then
+  cd $TMP || exit 1
+  git clone $GIT_IO || exit 1
+fi
+if [ ! -d $TMP/openfx-misc ]; then
+  cd $TMP || exit 1
+  git clone $GIT_MISC || exit 1
+fi
+if [ ! -d $TMP/openfx-arena ]; then
+  cd $TMP || exit 1
+  git clone $GIT_ARENA || exit 1
 fi
 
-#clone repos to get git version to check against NATRON_MASTER
-if [ ! -d $TMP/Natron ];then
-    cd $TMP || exit 1
-    git clone $GIT_NATRON || exit 1
-    cd Natron || exit 1
-    git checkout master || exit 1
+FINISHED=0
+while [ "$FINISHED" = "0" ]; do
+source $CWD/common.sh
+FAIL=0
+git pull origin $MASTER_BRANCH
+GIT_BRANCHES=`git branch -a| grep "origin" | sed 's/\// /g;/HEAD/d;/RB-1.1/d;/gh-pages/d;/coverity_scan/d;/workshop/d' | awk '{print $3}'`
+for CURRENT_BRANCH in $GIT_BRANCHES; do
+echo "=============== $CURRENT_BRANCH ==============="
+
+# parse commits hash
+if [ ! -f $CWD/commits-hash-${CURRENT_BRANCH}.sh ]; then
+    echo "#!/bin/sh" > $CWD/commits-hash-${CURRENT_BRANCH}.sh
+    echo "NATRON_DEVEL_GIT=#" >> $CWD/commits-hash-${CURRENT_BRANCH}.sh
+    echo "IOPLUG_DEVEL_GIT=#" >> $CWD/commits-hash-${CURRENT_BRANCH}.sh
+    echo "MISCPLUG_DEVEL_GIT=#" >> $CWD/commits-hash-${CURRENT_BRANCH}.sh
+    echo "ARENAPLUG_DEVEL_GIT=#" >> $CWD/commits-hash-${CURRENT_BRANCH}.sh
+    echo "CVPLUG_DEVEL_GIT=#" >> $CWD/commits-hash-${CURRENT_BRANCH}.sh
+    echo "NATRON_VERSION_NUMBER=#" >> $CWD/commits-hash-${CURRENT_BRANCH}.sh
+fi
+source $CWD/commits-hash-${CURRENT_BRANCH}.sh
+
+REPO_SUFFIX=snapshot-$CURRENT_BRANCH
+LOGS=$REPO_DIR_PREFIX$REPO_SUFFIX/logs
+
+# check natron
+BUILD_NATRON=0
+cd $TMP/Natron
+
+git fetch --all
+git reset --hard HEAD
+git clean -fd
+git checkout $CURRENT_BRANCH
+git pull
+
+if [ "$CURRENT_BRANCH" != "$MASTER_BRANCH" ]; then
+  echo "===> Checking $CURRENT_BRANCH ..."
+  if [ "$NATRON_DEVEL_GIT" != "#" ] && [ "$NATRON_DEVEL_GIT" != "" ]; then
+    echo "===> Checking for #snapshot ..."
+    GITV_NATRON=`git --no-pager log --pretty=oneline ${NATRON_DEVEL_GIT}..HEAD --grep=#snapshot --since=1.week |head -1|awk '{print $1}'`
+  else
+    echo "===> first-run, so don't build anything"
+    NATRON_LATEST_COMMIT=`git log |head -1|awk '{print $2}'`
+    echo "====> $NATRON_LATEST_COMMIT"
+    if [ -z "$NATRON_LATEST_COMMIT" ]; then
+      echo "===> latest commit is empty, should not happen!"
+      NATRON_LATEST_COMMIT="#"
+    fi
+    sed -i "" -e "s/NATRON_DEVEL_GIT=.*/NATRON_DEVEL_GIT=${NATRON_LATEST_COMMIT}/" $CWD/commits-hash-${CURRENT_BRANCH}.sh || exit 1
+    GITV_NATRON=""
+  fi
+else
+  GITV_NATRON=`git log |head -1|awk '{print $2}'`
+  MASTER_SNAPSHOT=`git --no-pager log --pretty=oneline ${NATRON_DEVEL_GIT}..HEAD --grep=#snapshot --since=1.week |head -1|awk '{print $1}'`
 fi
 
-if [ ! -d $TMP/openfx-io ];then
-    cd $TMP || exit 1
-    git clone $GIT_IO || exit 1
+ORIG_NATRON=$NATRON_DEVEL_GIT
+echo "==========> Natron $GITV_NATRON vs. $ORIG_NATRON"
+if [ "$GITV_NATRON" != "$ORIG_NATRON" -a "$FAIL" != "1" -a "$GITV_NATRON" != "" -a "$GITV_NATRON" != "#" ]; then
+  echo "===> Natron update needed!"
+  BUILD_NATRON=1
+fi
+if [ ! -z "$MASTER_SNAPSHOT" ]; then
+  echo "===> MASTER_BRANCH has an snapshot!"
+  echo "=====> MASTER_BRANCH $MASTER_SNAPSHOT vs. $ORIG_NATRON"
+  if [ "$MASTER_SNAPSHOT" != "$ORIG_NATRON" ]; then
+    echo "===> Natron upate needed!"
+    BUILD_NATRON=1
+  fi
 fi
 
-if [ ! -d $TMP/openfx-misc ];then
-    cd $TMP || exit 1
-    git clone $GIT_MISC || exit 1
+BUILD_IO=0
+if [ "$CURRENT_BRANCH" = "$MASTER_BRANCH" ]; then
+if [ "$FAIL" != "1" ]; then
+  cd $TMP/openfx-io
+
+  git fetch --all 
+  git reset --hard HEAD
+  git clean -fd
+  git checkout $MASTER_BRANCH
+  git pull 
+
+  GITV_IO=`git log|head -1|awk '{print $2}'`
+  ORIG_IO=$IOPLUG_DEVEL_GIT
+  echo "==========> IO $GITV_IO vs. $ORIG_IO"
+  if [ "$GITV_IO" != "$ORIG_IO" -a "$FAIL" != "1" ]; then
+    echo "===> IO update needed!"
+    BUILD_IO=1
+  fi
+fi
 fi
 
-if [ ! -d $TMP/openfx-arena ];then
-    cd $TMP || exit 1
-    git clone $GIT_ARENA || exit 1
+BUILD_MISC=0
+if [ "$CURRENT_BRANCH" = "$MASTER_BRANCH" ]; then
+if [ "$FAIL" != "1" ]; then
+  cd $TMP/openfx-misc
+
+  git fetch --all
+  git reset --hard HEAD
+  git clean -fd
+  git checkout $MASTER_BRANCH
+  git pull
+
+  GITV_MISC=`git log|head -1|awk '{print $2}'`
+  ORIG_MISC=$MISCPLUG_DEVEL_GIT
+  echo "==========> Misc $GITV_MISC vs. $ORIG_MISC"
+  if [ "$GITV_MISC" != "$ORIG_MISC" -a "$FAIL" != "1" ]; then
+    echo "===> Misc update needed!"
+    BUILD_MISC=1
+  fi
 fi
-#if [ ! -d $TMP/openfx-opencv ]; then
-#  cd $TMP || exit 1
-#  git clone $GIT_OPENCV || exit 1
-#fi
-
-if [ ! -f $CWD/commits-hash.sh ]; then
-    touch $CWD/commits-hash.sh
-    echo "#!/bin/sh" >> $CWD/commits-hash.sh
-    echo "NATRON_DEVEL_GIT=#" >> $CWD/commits-hash.sh
-    echo "IOPLUG_DEVEL_GIT=#" >> $CWD/commits-hash.sh
-    echo "MISCPLUG_DEVEL_GIT=#" >> $CWD/commits-hash.sh
-    echo "ARENAPLUG_DEVEL_GIT=#" >> $CWD/commits-hash.sh
-    echo "CVPLUG_DEVEL_GIT=#" >> $CWD/commits-hash.sh
-    echo "NATRON_VERSION_NUMBER=#" >> $CWD/commits-hash.sh
 fi
 
+BUILD_ARENA=0
+if [ "$CURRENT_BRANCH" = "$MASTER_BRANCH" ]; then
+if [ "$FAIL" != "1" ]; then
+  cd $TMP/openfx-arena
 
-#infinite loop with 1min sleeps
-while true; do
+  git fetch --all
+  git reset --hard HEAD
+  git clean -fd
+  git checkout $MASTER_BRANCH
+  git pull
 
-    source $CWD/commits-hash.sh || exit 1
+  GITV_ARENA=`git log|head -1|awk '{print $2}'`
+  ORIG_ARENA=$ARENAPLUG_DEVEL_GIT
+  echo "==========> Arena $GITV_ARENA vs. $ORIG_ARENA"
+  if [ "$GITV_ARENA" != "$ORIG_ARENA" -a "$FAIL" != "1" ]; then
+    echo "===> Arena update needed!"
+    BUILD_ARENA=1
+  fi
+fi
+fi
 
-    #Sync all scripts except snapshot.sh in case modifs were made
-    git pull origin master
+cd $CWD || exit 1
+if [ "$FAIL" != "1" ]; then
 
-    FAIL=0
 
-    BUILD_NATRON=0
-    BUILD_IO=0
-    BUILD_MISC=0
+if [ "$CURRENT_BRANCH" != "$MASTER_BRANCH" ]; then
+  if [ "$BUILD_NATRON" = "1" ];  then
+    sed -i "s/NATRON_DEVEL_GIT=.*/NATRON_DEVEL_GIT=${GITV_NATRON}/" $CWD/commits-hash-$CURRENT_BRANCH.sh || exit 1
+    env COMMIT=$GITV CONFIG=relwithdebinfo BRANCH=$CURRENT_BRANCH BUILD_CONFIG=SNAPSHOT MKJOBS=$MKJOBS UPLOAD=1 NO_CLEAN=$NO_CLEAN ./build.sh
+  fi
+else
+  if [ "$BUILD_NATRON" = "1" -o "$BUILD_IO" = "1" -o "$BUILD_MISC" = "1" -o "$BUILD_ARENA" = "1" -o "$BUILD_CV" = "1" ]; then
 
-    cd $TMP/Natron
-    git fetch || FAIL=1
-    git merge origin/master || FAIL=1
-    GITV_NATRON=`git log|head -1|awk '{print $2}'`
-    ORIG_NATRON=$NATRON_DEVEL_GIT
-    echo "Natron $GITV_NATRON vs. $ORIG_NATRON"
-    if [ "$GITV_NATRON" != "$ORIG_NATRON" -a "$FAIL" != "1" ];then
-        echo "Natron update needed"
-        BUILD_NATRON=1
+    if [ ! -z "$MASTER_SNAPSHOT" ]; then
+      DO_SYNC=1
+    else
+      DO_SYNC=0
     fi
 
-    echo $FAIL
-
-    if [ "$FAIL" != "1" ]; then
-        cd $TMP/openfx-io
-        git fetch || FAIL=1
-        git merge origin/master || FAIL=1
-        GITV_IO=`git log|head -1|awk '{print $2}'`
-        ORIG_IO=$IOPLUG_DEVEL_GIT
-        echo "openfx-io $GITV_IO vs. $ORIG_IO"
-        if [ "$GITV_IO" != "$ORIG_IO" -a "$FAIL" != "1" ];then
-            echo "openfx-io update needed"
-            BUILD_IO=1
-        fi
-    fi
-
-    echo $FAIL
-
-    if [ "$FAIL" != "1" ]; then
-        cd $TMP/openfx-misc
-        git fetch || FAIL=1
-        git merge origin/master || FAIL=1
-        GITV_MISC=`git log|head -1|awk '{print $2}'`
-        ORIG_MISC=$MISCPLUG_DEVEL_GIT
-        echo "openfx-misc $GITV_MISC vs. $ORIG_MISC"
-        if [ "$GITV_MISC" != "$ORIG_MISC" -a "$FAIL" != "1" ];then
-            echo "openfx-misc update needed"
-            BUILD_MISC=1
-        fi
-    fi
-
-    if [ "$FAIL" != "1" ]; then
-        cd $TMP/openfx-arena
-        git fetch || FAIL=1
-        git merge origin/master || FAIL=1
-        ARENAV_MISC=`git log|head -1|awk '{print $2}'`
-        ORIG_ARENA=$ARENAPLUG_DEVEL_GIT
-        echo "openfx-arena $ARENAV_MISC vs. $ORIG_ARENA"
-        if [ "$ARENAV_MISC" != "$ORIG_ARENA" -a "$FAIL" != "1" ];then
-            echo "openfx-arena update needed"
-            BUILD_ARENA=1
-        fi
-    fi
+    env CONFIG=relwithdebinfo BRANCH=$CURRENT_BRANCH BUILD_CONFIG=SNAPSHOT MKJOBS=$MKJOBS UPLOAD=$DO_SYNC NO_CLEAN=$NO_CLEAN ./build.sh
+  fi
+fi
 
 
-    echo $FAIL
+fi
 
-    cd $CWD || FAIL=1
-    if [ "$FAIL" != 1 ]; then
-        if [ "$BUILD_NATRON" = "1" -o "$BUILD_IO" = "1" -o "$BUILD_MISC" = "1" -o "$BUILD_ARENA" = "1" ]; then
-
-            GIT_COMMENT=`( cd $TMP/Natron ; git --no-pager log ${NATRON_DEVEL_GIT}..HEAD)`
-            GIT_SYNC=`echo $GIT_COMMENT|grep "#snapshot"`
-            if [ "$GIT_SYNC" = "" ]; then
-              GIT_COMMENT=`( cd $TMP/openfx-io ; git --no-pager log ${IOPLUG_DEVEL_GIT}..HEAD)`
-              GIT_SYNC=`echo $GIT_COMMENT|grep "#snapshot"`
-            fi
-            if [ "$GIT_SYNC" = "" ]; then
-              GIT_COMMENT=`( cd $TMP/openfx-misc ; git --no-pager log ${MISCPLUG_DEVEL_GIT}..HEAD)`
-              GIT_SYNC=`echo $GIT_COMMENT|grep "#snapshot"`
-            fi
-            if [ "$GIT_SYNC" = "" ]; then
-              GIT_COMMENT=`( cd $TMP/openfx-arena ; git --no-pager log ${ARENAPLUG_DEVEL_GIT}..HEAD)`
-              GIT_SYNC=`echo $GIT_COMMENT|grep "#snapshot"`
-            fi
-
-            if [ "$GIT_SYNC" != "" ]; then
-              DO_SYNC=1
-            else
-              DO_SYNC=0
-            fi
-            env CONFIG=relwithdebinfo BRANCH=master BUILD_CONFIG=SNAPSHOT MKJOBS=$MKJOBS UPLOAD=$DO_SYNC NO_CLEAN=$NO_CLEAN ./build.sh || FAIL=1
-            echo $FAIL
-        fi
-    fi
-
-    echo "Idle"
-    sleep 60
-done
+done # end branch loop
+if [ "$NO_LOOP" = "1" ]; then
+  FINISHED=1
+else
+  echo "Idle ..."
+  sleep 60
+fi
+done # end main loop
