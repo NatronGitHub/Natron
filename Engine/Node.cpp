2c80751ee6bc10cb09daf7f7f6f8099d2f748f2a
Alexandre Gauthier <alexandre230@msn.com>
2016-05-18 15:11:53 +0200

    Fix a bug where the separator in knobs properties before the channels selection could be misplaced in some plug-ins

--- Engine/Node.cpp
+++ Engine/Node.cpp
@@ -3376,7 +3376,7 @@ Node::createMaskSelectors(const std::vector<std::pair<bool, bool> >& hasMaskChan
             channel->setAddNewLine(false);
         }
         if (!*lastKnobCreated) {
-            *lastKnobCreated = channel;
+            *lastKnobCreated = enabled;
         }
 
         _imp->maskSelectors[i] = sel;
