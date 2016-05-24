3af56d6107cc5f8742a04d6cb66bde7126d9a173
Alexandre Gauthier <alexandre230@msn.com>
2016-05-24 16:38:23 +0200

    OfxImageEffectInstance: implement getEffectDuration()

--- Engine/OfxImageEffectInstance.cpp
+++ Engine/OfxImageEffectInstance.cpp
@@ -322,11 +322,20 @@ double
 OfxImageEffectInstance::getEffectDuration() const
 {
     assert( getOfxEffectInstance() );
-#ifdef DEBUG
-#pragma message WARN("getEffectDuration unimplemented, should we store the previous result to getTimeDomain?")
-#endif
-
-    return 1.0;
+    NodePtr node = getOfxEffectInstance()->getNode();
+    if (!node) {
+        return 0;
+    }
+    int firstFrame,lastFrame;
+    bool lifetimeEnabled = node->isLifetimeActivated(&firstFrame, &lastFrame);
+    if (lifetimeEnabled) {
+        return std::max(double(lastFrame - firstFrame) + 1., 1.);
+    } else {
+        // return the project duration if the effect has no lifetime
+        double projFirstFrame, projLastFrame;
+        node->getApp()->getProject()->getFrameRange(&projFirstFrame, &projLastFrame);
+        return std::max(projLastFrame - projFirstFrame + 1., 1.);
+    }
 }
 
 // For an instance, this is the frame rate of the project the effect is in.
