191a82ad1c124d786c5c51b225d055d1624183e8
Ole-André Rodlie <ole-andre.rodlie@inria.fr>
2016-06-21 11:05:20 +0200

    ReadNode: support pdf

--- Engine/ReadNode.cpp
+++ Engine/ReadNode.cpp
@@ -185,7 +185,8 @@ ReadNode::isBundledReader(const std::string& pluginID,
                boost::iequals(pluginID, PLUGINID_OFX_READMISC) ||
                boost::iequals(pluginID, PLUGINID_OFX_READORA) ||
                boost::iequals(pluginID, PLUGINID_OFX_READCDR) ||
-               boost::iequals(pluginID, PLUGINID_OFX_READPNG);
+               boost::iequals(pluginID, PLUGINID_OFX_READPNG) ||
+               boost::iequals(pluginID, PLUGINID_OFX_READPDF);
     }
 
     return pluginID == PLUGINID_OFX_READOIIO ||
@@ -197,7 +198,8 @@ ReadNode::isBundledReader(const std::string& pluginID,
            pluginID == PLUGINID_OFX_READMISC ||
            pluginID == PLUGINID_OFX_READORA ||
            pluginID == PLUGINID_OFX_READCDR ||
-           pluginID == PLUGINID_OFX_READPNG;
+           pluginID == PLUGINID_OFX_READPNG ||
+           pluginID == PLUGINID_OFX_READPDF;
 }
 
 bool
