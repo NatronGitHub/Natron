b9adebdcecf4ddefbd56f913a5d36958666d17f3
Alexandre Gauthier <alexandre230@msn.com>
2016-07-02 11:56:22 +0200

    Windows: If Natron fails to meet viewer requirements on startup, invite the user to install  software OpenGL

--- Engine/AppManagerPrivate.cpp
+++ Engine/AppManagerPrivate.cpp
@@ -768,6 +768,10 @@ AppManagerPrivate::initGl(bool checkRenderingReq)
             viewerReq.error += tr("Your OpenGL version ");
             viewerReq.error += glVersion;
         }
+#ifdef __NATRON_WIN32__
+        viewerReq.error += tr("To fix this: Re-start the installer, select \"Update Components\" and then install the \"Software OpenGL\" component.\n");
+        viewerReq.error += tr("Another solution is to manually copy opengl32.dll located in the \"bin\mesa\" directory of your %1 installation to the \"bin\" directory.").arg(QLatin1String(NATRON_APPLICATION_NAME));
+#endif
 
         viewerReq.hasRequirements = false;
         renderingReq.hasRequirements = false;
