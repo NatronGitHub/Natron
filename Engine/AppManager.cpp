685aa9c82f45095dc7531cd7d703e675ca7f5101
Alexandre Gauthier <alexandre230@msn.com>
2016-10-05 16:01:12 +0200

    do not use strdup but strncpy instead

--- Engine/AppManager.cpp
+++ Engine/AppManager.cpp
@@ -2881,7 +2881,9 @@ AppManager::initPython(int argc,
 #ifndef IS_PYTHON_2
         _imp->args[i] = char2wchar(argv[i]);
 #else
-        _imp->args[i] = strdup(argv[i]); // free'd in ~AppManagerPrivate()
+        QString s = QString::fromUtf8(argv[i]);
+        std::string stdS = s.toStdString();
+        strncpy(_imp->args[i], stdS.c_str(), stdS.size());
 #endif
     }
 
