9abf687a196f3fb38cae4c752a1bcc8dcc3c51bb
Ole-André Rodlie <ole-andre.rodlie@inria.fr>
2016-10-10 14:45:16 +0200

    Gui: force style on unix (avoid adding -style on cmdline), #1464

--- Gui/GuiApplicationManager.cpp
+++ Gui/GuiApplicationManager.cpp
@@ -845,6 +845,15 @@ GuiApplicationManager::initGui(const CLArgs& args)
 {
     QSettings settings( QString::fromUtf8(NATRON_ORGANIZATION_NAME), QString::fromUtf8(NATRON_APPLICATION_NAME) );
 
+#ifdef __NATRON_UNIX__
+#ifndef __NATRON_OSX__
+#if QT_VERSION < 0x050000
+    // workaround for issues with KDE4
+    QApplication::setStyle( QString::fromUtf8("plastique") );
+#endif
+#endif
+#endif
+
     //load custom fonts
     QString fontResource = QString::fromUtf8(":/Resources/Fonts/%1.ttf");
     QStringList fontFilenames;
