3841b52e6dff81c2ec12713d8edbe7e9e4750eaa
Frédéric Devernay <frederic.devernay@m4x.org>
2016-11-10 17:50:08 +0100

    Gui: fix warning

--- Gui/Gui.cpp
+++ Gui/Gui.cpp
@@ -754,7 +754,7 @@ Gui::dockClicked()
 {
     raise();
     show();
-    setWindowState(windowState() & ~Qt::WindowMinimized | Qt::WindowActive);
+    setWindowState( (windowState() & ~Qt::WindowMinimized) | Qt::WindowActive);
 }
 #endif
 
