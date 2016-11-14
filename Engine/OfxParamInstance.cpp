971fb2319b4ac529a3e07633ecb4195f09880744
Alexandre Gauthier <alexandre230@msn.com>
2016-11-14 15:48:12 +0100

    Fix #1476

--- Engine/OfxParamInstance.cpp
+++ Engine/OfxParamInstance.cpp
@@ -2364,8 +2364,6 @@ void
 OfxDouble2DInstance::setRange()
 {
     DYNAMIC_PROPERTY_CHECK();
-    std::vector<double> displayMins(2);
-    std::vector<double> displayMaxs(2);
     KnobDoublePtr knob = _knob.lock();
 
     std::vector<double> mins(2);
@@ -2373,8 +2371,8 @@ OfxDouble2DInstance::setRange()
     _properties.getDoublePropertyN(kOfxParamPropMin, &mins[0], mins.size());
     _properties.getDoublePropertyN(kOfxParamPropMax, &maxs[0], maxs.size());
     for (int i = 0; i < 2; ++i) {
-        knob->setMinimum(displayMins[i], i + _startIndex);
-        knob->setMaximum(displayMaxs[i], i + _startIndex);
+        knob->setMinimum(mins[i], i + _startIndex);
+        knob->setMaximum(maxs[i], i + _startIndex);
     }
 }
 
@@ -2611,16 +2609,14 @@ OfxInteger2DInstance::setRange()
 {
     DYNAMIC_PROPERTY_CHECK();
 
-    std::vector<int> displayMins(2);
-    std::vector<int> displayMaxs(2);
     KnobIntPtr knob = _knob.lock();
     std::vector<int> mins(2);
     std::vector<int> maxs(2);
     _properties.getIntPropertyN(kOfxParamPropMin, &mins[0], mins.size());
     _properties.getIntPropertyN(kOfxParamPropMax, &maxs[0], maxs.size());
     for (int i = 0; i < 2; ++i) {
-        knob->setMinimum(displayMins[i], i + _startIndex);
-        knob->setMaximum(displayMaxs[i], i + _startIndex);
+        knob->setMinimum(mins[i], i + _startIndex);
+        knob->setMaximum(maxs[i], i + _startIndex);
     }
 }
 
