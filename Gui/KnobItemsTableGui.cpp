c4c3886f59ca04684b1ac0e555cf360cbb2f4868
Frédéric Devernay <frederic.devernay@m4x.org>
2017-01-01 14:00:08 +0100

    fix build

--- Gui/KnobItemsTableGui.cpp
+++ Gui/KnobItemsTableGui.cpp
@@ -521,7 +521,7 @@ KnobItemsTableGui::KnobItemsTableGui(const KnobItemsTablePtr& table, DockablePan
     int nCols = table->getColumnsCount();
 
     KnobItemsTable::KnobItemsTableTypeEnum knobTableType = table->getType();
-    TableModel::TableModelTypeEnum type = KnobItemsTable::eKnobItemsTableTypeTable;
+    TableModel::TableModelTypeEnum type = TableModel::eTableModelTypeTable;
     switch (knobTableType) {
         case KnobItemsTable::eKnobItemsTableTypeTable:
             type = TableModel::eTableModelTypeTable;
