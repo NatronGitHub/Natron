3b77bc96975b885c00e486f750c818835489f7a4
Frédéric Devernay <frederic.devernay@m4x.org>
2017-01-30 10:59:37 +0100

    Global: use the multiplane2 plugin branches

--- Global/plugin-branches.sh
+++ Global/plugin-branches.sh
@@ -1,5 +1,5 @@
 # this file is sourced by the build-plugins.sh script if it exists
-IO_BRANCH=master
-MISC_BRANCH=master
-ARENA_BRANCH=master
+IO_BRANCH=multiplane2
+MISC_BRANCH=multiplane2
+ARENA_BRANCH=multiplane2
 CV_BRANCH=master
