fbc44d993219ecccc22f10856346315fa2b81270
Frédéric Devernay <frederic.devernay@m4x.org>
2017-03-24 14:08:40 +0100

    ProcInfo.h : do not include Python.h

--- Global/ProcInfo.h
+++ Global/ProcInfo.h
@@ -19,11 +19,7 @@
 #ifndef NATRON_GLOBAL_PROCINFO_H
 #define NATRON_GLOBAL_PROCINFO_H
 
-// ***** BEGIN PYTHON BLOCK *****
-// from <https://docs.python.org/3/c-api/intro.html#include-files>:
-// "Since Python may define some pre-processor definitions which affect the standard headers on some systems, you must include Python.h before any standard headers are included."
-#include <Python.h>
-// ***** END PYTHON BLOCK *****
+// Python.h not included here, because this is used by CrashReporter, which does not use python
 
 #include "Global/Macros.h"
 
