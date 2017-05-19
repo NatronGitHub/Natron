7327b060311b83dc65a4505f73dc3ae7e8c4cd39
Alexandre Gauthier <alexandre230@msn.com>
2017-05-19 12:47:56 +0200

    printAsTime: bug fix

--- Engine/Timer.cpp
+++ Engine/Timer.cpp
@@ -139,7 +139,7 @@ Timer::printAsTime(const double timeInSeconds,
         if (timeInSeconds >= day) {
             ret.append( QLatin1Char(' ') );
         }
-        double hourRemaining = timeInSeconds / hour;
+        double hourRemaining = timeRemain / hour;
         double floorHour = std::floor(hourRemaining);
         if (floorHour > 0) {
             ret.append( ( (floorHour > 1) ? tr("%1 hours") : tr("%1 hour") ).arg( QString::number(floorHour) ) );
@@ -153,7 +153,7 @@ Timer::printAsTime(const double timeInSeconds,
         if (timeInSeconds >= hour) {
             ret.append( QLatin1Char(' ') );
         }
-        double minRemaining = timeInSeconds / min;
+        double minRemaining = timeRemain / min;
         double floorMin = std::floor(minRemaining);
         if (floorMin > 0) {
             ret.append( ( (floorMin > 1) ? tr("%1 minutes") : tr("%1 minute") ).arg( QString::number(floorMin) ) );
