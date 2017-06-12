9f5f505461e89d352c5a2ba69c21eb2292d40b7e
Alexandre Gauthier <alexandre230@msn.com>
2017-06-12 08:48:46 +0200

    Merge commit 16f2db71604c8c21d72c0bf5703ec1cf210f57d6

--- Gui/NodeGraphPrivate10.cpp
+++ Gui/NodeGraphPrivate10.cpp
@@ -124,7 +124,6 @@ NodeGraphPrivate::pasteNodesInternal(const std::list<std::pair<NodePtr, SERIALIZ
         serializationList.push_back(it->second);
     }
     
-
     // Create nodes
     std::list<std::pair<NodePtr, SERIALIZATION_NAMESPACE::NodeSerializationPtr > > createdNodes;
     Project::restoreGroupFromSerialization(serializationList, group.lock(), &createdNodes);
