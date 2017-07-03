4ad036d5448b306dfb0b9c5bebdcf4aad8f8c73f
Frédéric Devernay <frederic.devernay@m4x.org>
2017-07-03 17:39:32 +0200

    openfx: use the master branch

--- Global/plugin-branches.sh
+++ Global/plugin-branches.sh
@@ -1,5 +1,5 @@
 # this file is sourced by the build-plugins.sh script if it exists
-IO_BRANCH=multiplane2
-MISC_BRANCH=multiplane2
-ARENA_BRANCH=multiplane2
+IO_BRANCH=master
+MISC_BRANCH=master
+ARENA_BRANCH=master
 CV_BRANCH=master
