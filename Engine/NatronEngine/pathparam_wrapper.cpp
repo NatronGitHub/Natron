NatronGitHub/Natron

commit 8b2b55162e35bbbe9acc3685973b07bef540cb3d

reduce warnings

Engine/PyAppInstance.h
@@ -44,8 +44,8 @@ CLANG_DIAG_ON(uninitialized)
 #include "Engine/PyNode.h"
 #include "Engine/EngineFwd.h"
 
-NATRON_NAMESPACE_ENTER;
-NATRON_PYTHON_NAMESPACE_ENTER;
+NATRON_NAMESPACE_ENTER
+NATRON_PYTHON_NAMESPACE_ENTER
 
 class AppSettings
 {
@@ -332,7 +332,7 @@ protected:
     boost::shared_ptr<NodeCollection> getCollectionFromGroup(Group* group) const;
 };
 
-NATRON_PYTHON_NAMESPACE_EXIT;
-NATRON_NAMESPACE_EXIT;
+NATRON_PYTHON_NAMESPACE_EXIT
+NATRON_NAMESPACE_EXIT
 
 #endif // Engine_AppInstanceWrapper_h

Engine/PyExprUtils.h
@@ -31,8 +31,8 @@
 
 #include "Engine/PyParameter.h"
 
-NATRON_NAMESPACE_ENTER;
-NATRON_PYTHON_NAMESPACE_ENTER;
+NATRON_NAMESPACE_ENTER
+NATRON_PYTHON_NAMESPACE_ENTER
 
 
 class ExprUtils
@@ -121,8 +121,8 @@ public:
     static double pnoise(const Double3DTuple& p, const Double3DTuple& period);
 };
 
-NATRON_PYTHON_NAMESPACE_EXIT;
-NATRON_NAMESPACE_EXIT;
+NATRON_PYTHON_NAMESPACE_EXIT
+NATRON_NAMESPACE_EXIT
 
 
 #endif // PYNOISE_H

Engine/PyGlobalFunctions.h
@@ -38,8 +38,8 @@
 
 #include "Engine/EngineFwd.h"
 
-NATRON_NAMESPACE_ENTER;
-NATRON_PYTHON_NAMESPACE_ENTER;
+NATRON_NAMESPACE_ENTER
+NATRON_PYTHON_NAMESPACE_ENTER
 
 class PyCoreApplication
 {
@@ -227,8 +227,8 @@ public:
     }
 };
 
-NATRON_PYTHON_NAMESPACE_EXIT;
-NATRON_NAMESPACE_EXIT;
+NATRON_PYTHON_NAMESPACE_EXIT
+NATRON_NAMESPACE_EXIT
 
 #if defined(PYSIDE_H) && defined(PYSIDE_OLD)
 namespace PySide {

Engine/PyNode.h
@@ -43,8 +43,8 @@
 #include "Engine/RectD.h"
 #include "Engine/EngineFwd.h"
 
-NATRON_NAMESPACE_ENTER;
-NATRON_PYTHON_NAMESPACE_ENTER;
+NATRON_NAMESPACE_ENTER
+NATRON_PYTHON_NAMESPACE_ENTER
 
 class ImageLayer
 {
@@ -387,7 +387,7 @@ public:
     void setPagesOrder(const QStringList& pages);
 };
 
-NATRON_PYTHON_NAMESPACE_EXIT;
-NATRON_NAMESPACE_EXIT;
+NATRON_PYTHON_NAMESPACE_EXIT
+NATRON_NAMESPACE_EXIT
 
 #endif // NODEWRAPPER_H

Engine/PyNodeGroup.h
@@ -35,8 +35,8 @@
 
 #include "Engine/EngineFwd.h"
 
-NATRON_NAMESPACE_ENTER;
-NATRON_PYTHON_NAMESPACE_ENTER;
+NATRON_NAMESPACE_ENTER
+NATRON_PYTHON_NAMESPACE_ENTER
 
 class Group
 {
@@ -69,7 +69,7 @@ public:
     std::list<Effect*> getChildren() const;
 };
 
-NATRON_PYTHON_NAMESPACE_EXIT;
-NATRON_NAMESPACE_EXIT;
+NATRON_PYTHON_NAMESPACE_EXIT
+NATRON_NAMESPACE_EXIT
 
 #endif // Engine_NodeGroupWrapper_h

Engine/PyParameter.h
@@ -41,8 +41,8 @@
 #include "Engine/KnobFile.h"
 #include "Engine/EngineFwd.h"
 
-NATRON_NAMESPACE_ENTER;
-NATRON_PYTHON_NAMESPACE_ENTER;
+NATRON_NAMESPACE_ENTER
+NATRON_PYTHON_NAMESPACE_ENTER
 
 class Param
 {
@@ -1192,7 +1192,7 @@ public:
     void setDefaultCurvesFromCurrentCurves();
 };
 
-NATRON_PYTHON_NAMESPACE_EXIT;
-NATRON_NAMESPACE_EXIT;
+NATRON_PYTHON_NAMESPACE_EXIT
+NATRON_NAMESPACE_EXIT
 
 #endif // Engine_ParameterWrapper_h

Engine/PyRoto.h
@@ -38,8 +38,8 @@
 #include "Engine/PyParameter.h"
 #include "Engine/EngineFwd.h"
 
-NATRON_NAMESPACE_ENTER;
-NATRON_PYTHON_NAMESPACE_ENTER;
+NATRON_NAMESPACE_ENTER
+NATRON_PYTHON_NAMESPACE_ENTER
 
 class Layer; // defined below
 
@@ -191,7 +191,7 @@ private:
     boost::shared_ptr<RotoContext> _ctx;
 };
 
-NATRON_PYTHON_NAMESPACE_EXIT;
-NATRON_NAMESPACE_EXIT;
+NATRON_PYTHON_NAMESPACE_EXIT
+NATRON_NAMESPACE_EXIT
 
 #endif // ROTOWRAPPER_H

Engine/PyTracker.h
@@ -37,8 +37,8 @@
 #include "Engine/PyParameter.h"
 #include "Engine/EngineFwd.h"
 
-NATRON_NAMESPACE_ENTER;
-NATRON_PYTHON_NAMESPACE_ENTER;
+NATRON_NAMESPACE_ENTER
+NATRON_PYTHON_NAMESPACE_ENTER
 
 class Track
 {
@@ -100,7 +100,7 @@ private:
     boost::weak_ptr<TrackerContext> _ctx;
 };
 
-NATRON_PYTHON_NAMESPACE_EXIT;
-NATRON_NAMESPACE_EXIT;
+NATRON_PYTHON_NAMESPACE_EXIT
+NATRON_NAMESPACE_EXIT
 
 #endif // Engine_PyTracker_H

commit bc1445ae9b858cba6287661b110615274947d693

Python/shiboken: All files included by shiboken must have a namespace macro (such as NATRON_NAMESPACE_ENTER) appended with a ';' character, otherwise shiboken fails to generate code. Bug was introduced in https://github.com/MrKepzie/Natron/commit/8b2b55162e35bbbe9acc3685973b07bef540cb3d

Engine/NatronEngine/pathparam_wrapper.cpp
@@ -152,7 +152,7 @@ static PyObject* Sbk_PathParamFunc_setTable(PyObject* self, PyObject* pyArg)
 
 
                 PyObject* subList = PyList_GET_ITEM(pyArg,i);
-                if (!subList) {
+                if (!subList || !PyList_Check(subList)) {
                     PyErr_SetString(PyExc_TypeError, "table must be a list of list objects.");
                     return 0;
                 }
@@ -161,8 +161,14 @@ static PyObject* Sbk_PathParamFunc_setTable(PyObject* self, PyObject* pyArg)
 
                 for (int j = 0; j < subSize; ++j) {
                     PyObject* pyString = PyList_GET_ITEM(subList,j);
-                    if ( PyUnicode_Check(pyString) ) {
-
+                    if ( PyString_Check(pyString) ) {
+                        char* buf = PyString_AsString(pyString);
+                        if (buf) {
+                            std::string ret;
+                            ret.append(buf);
+                            rowVec[j] = ret;
+                            }
+                    } else if (PyUnicode_Check(pyString) ) {
                         PyObject* utf8pyobj = PyUnicode_AsUTF8String(pyString); // newRef
                         if (utf8pyobj) {
                             char* cstr = PyBytes_AS_STRING(utf8pyobj); // Borrowed pointer
@@ -173,6 +179,7 @@ static PyObject* Sbk_PathParamFunc_setTable(PyObject* self, PyObject* pyArg)
                         }
                     }
                 }
+                table.push_back(rowVec);
             }
 
             cppSelf->setTable(table);

Engine/PyAppInstance.h
@@ -44,8 +44,8 @@ CLANG_DIAG_ON(uninitialized)
 #include "Engine/PyNode.h"
 #include "Engine/EngineFwd.h"
 
-NATRON_NAMESPACE_ENTER
-NATRON_PYTHON_NAMESPACE_ENTER
+NATRON_NAMESPACE_ENTER;
+NATRON_PYTHON_NAMESPACE_ENTER;
 
 class AppSettings
 {
@@ -332,7 +332,7 @@ protected:
     boost::shared_ptr<NodeCollection> getCollectionFromGroup(Group* group) const;
 };
 
-NATRON_PYTHON_NAMESPACE_EXIT
-NATRON_NAMESPACE_EXIT
+NATRON_PYTHON_NAMESPACE_EXIT;
+NATRON_NAMESPACE_EXIT;
 
 #endif // Engine_AppInstanceWrapper_h

Engine/PyExprUtils.h
@@ -31,8 +31,8 @@
 
 #include "Engine/PyParameter.h"
 
-NATRON_NAMESPACE_ENTER
-NATRON_PYTHON_NAMESPACE_ENTER
+NATRON_NAMESPACE_ENTER;
+NATRON_PYTHON_NAMESPACE_ENTER;
 
 
 class ExprUtils
@@ -121,8 +121,8 @@ public:
     static double pnoise(const Double3DTuple& p, const Double3DTuple& period);
 };
 
-NATRON_PYTHON_NAMESPACE_EXIT
-NATRON_NAMESPACE_EXIT
+NATRON_PYTHON_NAMESPACE_EXIT;
+NATRON_NAMESPACE_EXIT;
 
 
 #endif // PYNOISE_H

Engine/PyGlobalFunctions.h
@@ -38,8 +38,8 @@
 
 #include "Engine/EngineFwd.h"
 
-NATRON_NAMESPACE_ENTER
-NATRON_PYTHON_NAMESPACE_ENTER
+NATRON_NAMESPACE_ENTER;
+NATRON_PYTHON_NAMESPACE_ENTER;
 
 class PyCoreApplication
 {
@@ -227,8 +227,8 @@ public:
     }
 };
 
-NATRON_PYTHON_NAMESPACE_EXIT
-NATRON_NAMESPACE_EXIT
+NATRON_PYTHON_NAMESPACE_EXIT;
+NATRON_NAMESPACE_EXIT;
 
 #if defined(PYSIDE_H) && defined(PYSIDE_OLD)
 namespace PySide {

Engine/PyNode.h
@@ -43,8 +43,8 @@
 #include "Engine/RectD.h"
 #include "Engine/EngineFwd.h"
 
-NATRON_NAMESPACE_ENTER
-NATRON_PYTHON_NAMESPACE_ENTER
+NATRON_NAMESPACE_ENTER;
+NATRON_PYTHON_NAMESPACE_ENTER;
 
 class ImageLayer
 {

Engine/PyNodeGroup.h
@@ -35,8 +35,8 @@
 
 #include "Engine/EngineFwd.h"
 
-NATRON_NAMESPACE_ENTER
-NATRON_PYTHON_NAMESPACE_ENTER
+NATRON_NAMESPACE_ENTER;
+NATRON_PYTHON_NAMESPACE_ENTER;
 
 class Group
 {

Engine/PyParameter.h
@@ -41,8 +41,8 @@
 #include "Engine/KnobFile.h"
 #include "Engine/EngineFwd.h"
 
-NATRON_NAMESPACE_ENTER
-NATRON_PYTHON_NAMESPACE_ENTER
+NATRON_NAMESPACE_ENTER;
+NATRON_PYTHON_NAMESPACE_ENTER;
 
 class Param
 {
@@ -1194,7 +1194,7 @@ public:
     void setDefaultCurvesFromCurrentCurves();
 };
 
-NATRON_PYTHON_NAMESPACE_EXIT
-NATRON_NAMESPACE_EXIT
+NATRON_PYTHON_NAMESPACE_EXIT;
+NATRON_NAMESPACE_EXIT;
 
 #endif // Engine_ParameterWrapper_h

Engine/PyRoto.h
@@ -38,8 +38,8 @@
 #include "Engine/PyParameter.h"
 #include "Engine/EngineFwd.h"
 
-NATRON_NAMESPACE_ENTER
-NATRON_PYTHON_NAMESPACE_ENTER
+NATRON_NAMESPACE_ENTER;
+NATRON_PYTHON_NAMESPACE_ENTER;
 
 class Layer; // defined below
 
@@ -191,7 +191,7 @@ private:
     boost::shared_ptr<RotoContext> _ctx;
 };
 
-NATRON_PYTHON_NAMESPACE_EXIT
-NATRON_NAMESPACE_EXIT
+NATRON_PYTHON_NAMESPACE_EXIT;
+NATRON_NAMESPACE_EXIT;
 
 #endif // ROTOWRAPPER_H

Engine/PyTracker.h
@@ -37,8 +37,8 @@
 #include "Engine/PyParameter.h"
 #include "Engine/EngineFwd.h"
 
-NATRON_NAMESPACE_ENTER
-NATRON_PYTHON_NAMESPACE_ENTER
+NATRON_NAMESPACE_ENTER;
+NATRON_PYTHON_NAMESPACE_ENTER;
 
 class Track
 {
@@ -100,7 +100,7 @@ private:
     boost::weak_ptr<TrackerContext> _ctx;
 };
 
-NATRON_PYTHON_NAMESPACE_EXIT
-NATRON_NAMESPACE_EXIT
+NATRON_PYTHON_NAMESPACE_EXIT;
+NATRON_NAMESPACE_EXIT;
 
 #endif // Engine_PyTracker_H

Engine/RectD.h
@@ -44,7 +44,7 @@
 GCC_DIAG_OFF(strict-overflow)
 #endif
 
-NATRON_NAMESPACE_ENTER
+NATRON_NAMESPACE_ENTER;
 
 class RectD
 {
@@ -344,7 +344,7 @@ operator!=(const RectD & b1,
            b1.top() != b2.top();
 }
 
-NATRON_NAMESPACE_EXIT
+NATRON_NAMESPACE_EXIT;
 
 Q_DECLARE_METATYPE(NATRON_NAMESPACE::RectD)
 

Engine/RectI.h
@@ -44,7 +44,7 @@
 GCC_DIAG_OFF(strict-overflow)
 #endif
 
-NATRON_NAMESPACE_ENTER
+NATRON_NAMESPACE_ENTER;
 
 /**
  * @brief A rectangle where x1 < x2 and y1 < y2 such as width() == (x2 - x1) && height() == (y2 - y1)
@@ -469,7 +469,7 @@ operator!=(const RectI & b1,
     return !(b1 == b2);
 }
 
-NATRON_NAMESPACE_EXIT
+NATRON_NAMESPACE_EXIT;
 
 Q_DECLARE_METATYPE(NATRON_NAMESPACE::RectI)
 

Engine/typesystem_engine.xml
@@ -217,7 +217,6 @@
     <enum-type name="PixmapEnum"/>
     <enum-type name="ValueChangedReasonEnum"/>
     <enum-type name="AnimationLevelEnum"/>
-    <enum-type name="ImageComponentsEnum"/>
     <enum-type name="ImageBitDepthEnum"/>
     <enum-type name="ImagePremultiplicationEnum"/>
     <enum-type name="ViewerCompositingOperatorEnum"/>
@@ -770,10 +769,10 @@
        </modify-function>
        
    </object-type>
-   
-   <value-type name="ImageLayer" copyable="true" hash-function="ImageLayer::getHash">
-   </value-type>
 
+    <value-type name="ImageLayer" copyable="true" hash-function="ImageLayer::getHash">
+    </value-type>
+    
     <object-type name="Group" copyable="false">
         <inject-documentation format="target">
             This is an abstract class, it is derived by 2 different classes:
@@ -1506,8 +1505,14 @@
 
                     for (int j = 0; j &lt; subSize; ++j) {
                         PyObject* pyString = PyList_GET_ITEM(subList,j);
-                        if ( PyString_Check(pyString) || PyUnicode_Check(pyString) ) {
-
+                        if ( PyString_Check(pyString) ) {
+                            char* buf = PyString_AsString(pyString);
+                            if (buf) {
+                                std::string ret;
+                                ret.append(buf);
+                                rowVec[j] = ret;
+                                }
+                        } else if (PyUnicode_Check(pyString) ) {
                             PyObject* utf8pyobj = PyUnicode_AsUTF8String(pyString); // newRef
                             if (utf8pyobj) {
                                 char* cstr = PyBytes_AS_STRING(utf8pyobj); // Borrowed pointer
@@ -1518,6 +1523,7 @@
                             }
                         }
                     }
+                    table.push_back(rowVec);
                 }
 
                 %CPPSELF.%FUNCTION_NAME(table);

Gui/NatronGui/natrongui_module_wrapper.cpp
@@ -39,8 +39,8 @@ void init_PyGuiApplication(PyObject* module);
 void init_GuiApp(PyObject* module);
 void init_PyViewer(PyObject* module);
 void init_PyTabWidget(PyObject* module);
-void init_PyPanel(PyObject* module);
 void init_PyModalDialog(PyObject* module);
+void init_PyPanel(PyObject* module);
 
 // Required modules' type and converter arrays.
 PyTypeObject** SbkPySide_QtGuiTypes;
@@ -508,8 +508,8 @@ SBK_MODULE_INIT_FUNCTION_BEGIN(NatronGui)
     init_GuiApp(module);
     init_PyViewer(module);
     init_PyTabWidget(module);
-    init_PyPanel(module);
     init_PyModalDialog(module);
+    init_PyPanel(module);
 
     // Register converter for type 'const std::map<QString,NodeCreationProperty*>&'.
     SbkNatronGuiTypeConverters[SBK_NATRONGUI_STD_MAP_QSTRING_NODECREATIONPROPERTYPTR_IDX] = Shiboken::Conversions::createConverter(&PyDict_Type, _conststd_map_QString_NodeCreationPropertyPTR_REF_CppToPython__conststd_map_QString_NodeCreationPropertyPTR_REF);

Gui/PyGlobalGui.h
@@ -41,8 +41,8 @@ CLANG_DIAG_ON(uninitialized)
 #include "Gui/GuiApplicationManager.h"
 #include "Gui/GuiFwd.h"
 
-NATRON_NAMESPACE_ENTER
-NATRON_PYTHON_NAMESPACE_ENTER
+NATRON_NAMESPACE_ENTER;
+NATRON_PYTHON_NAMESPACE_ENTER;
 
 class PyGuiApplication
     : public PyCoreApplication
@@ -121,7 +121,7 @@ public:
     }
 };
 
-NATRON_PYTHON_NAMESPACE_EXIT
-NATRON_NAMESPACE_EXIT
+NATRON_PYTHON_NAMESPACE_EXIT;
+NATRON_NAMESPACE_EXIT;
 
 #endif // GLOBAL_GUI_WRAPPER_H

Gui/PyGuiApp.h
@@ -35,8 +35,8 @@
 #include "Gui/GuiAppInstance.h"
 #include "Gui/GuiFwd.h"
 
-NATRON_NAMESPACE_ENTER
-NATRON_PYTHON_NAMESPACE_ENTER
+NATRON_NAMESPACE_ENTER;
+NATRON_PYTHON_NAMESPACE_ENTER;
 
 class PyViewer
 {
@@ -165,7 +165,7 @@ public:
     void renderBlocking(const std::list<Effect*>& effects, const std::list<int>& firstFrames, const std::list<int>& lastFrames, const std::list<int>& frameSteps);
 };
 
-NATRON_PYTHON_NAMESPACE_EXIT
-NATRON_NAMESPACE_EXIT
+NATRON_PYTHON_NAMESPACE_EXIT;
+NATRON_NAMESPACE_EXIT;
 
 #endif // GUIAPPWRAPPER_H

Gui/PythonPanels.h
@@ -45,8 +45,8 @@ CLANG_DIAG_ON(uninitialized)
 #include "Gui/PanelWidget.h"
 #include "Gui/GuiFwd.h"
 
-NATRON_NAMESPACE_ENTER
-NATRON_PYTHON_NAMESPACE_ENTER
+NATRON_NAMESPACE_ENTER;
+NATRON_PYTHON_NAMESPACE_ENTER;
 
 struct DialogParamHolderPrivate;
 class DialogParamHolder
@@ -215,7 +215,7 @@ public:
     QString getScriptName() const;
 };
 
-NATRON_PYTHON_NAMESPACE_EXIT
-NATRON_NAMESPACE_EXIT
+NATRON_PYTHON_NAMESPACE_EXIT;
+NATRON_NAMESPACE_EXIT;
 
 #endif // PYTHONPANELS_H