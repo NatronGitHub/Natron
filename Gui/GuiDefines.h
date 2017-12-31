4f4f41f9a4ff085bcb55c56061c852005261800f
Frédéric Devernay <frederic.devernay@m4x.org>
2017-12-31 18:48:30 +0100

    Gui: raise NATRON_MAX_RECENT_FILES to 10

--- Gui/GuiDefines.h
+++ Gui/GuiDefines.h
@@ -61,6 +61,6 @@
 #define NATRON_FONT_SIZE_13 13
 #define NATRON_FONT_SIZE_DEFAULT NATRON_FONT_SIZE_11 // the sliders font becomes undreadable below 11 on non-HiDPI mac displays
 
-#define NATRON_MAX_RECENT_FILES 5
+#define NATRON_MAX_RECENT_FILES 10 // 10 is the default is most apps
 
 #endif // Gui_GuiDefines_h
