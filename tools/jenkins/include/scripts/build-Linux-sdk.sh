2fe4f0db9235a28f39811d0aedd27ce02bc3bd15
fonkle <fonkle@gmx.com>
2019-04-29 15:32:18 +0200

    NatronSDK-Linux-fixes: fontconfig
    fontconfig did not build due to a missing uuid, which is part of util-linux, which is only built AFTER fontconfig.
    Switching the build order of fontconfig and util-linux in the script fixes this issue.

--- tools/jenkins/include/scripts/build-Linux-sdk.sh
+++ tools/jenkins/include/scripts/build-Linux-sdk.sh
@@ -1332,29 +1332,6 @@ env CFLAGS="$BF" CXXFLAGS="$BF" ./configure --prefix="$SDK_HOME" --disable-stati
     end_build "$FTYPE_TAR"
 fi
 
-# Install fontconfig
-# see http://www.linuxfromscratch.org/blfs/view/cvs/general/fontconfig.html
-FONTCONFIG_VERSION=2.13.0
-FONTCONFIG_TAR="fontconfig-${FONTCONFIG_VERSION}.tar.gz"
-FONTCONFIG_SITE="https://www.freedesktop.org/software/fontconfig/release"
-if [ "${REBUILD_FONTCONFIG:-}" = "1" ]; then
-    rm -f "$SDK_HOME"/lib/libfontconfig* || true
-    rm -rf "$SDK_HOME"/include/fontconfig || true
-    rm -f "$SDK_HOME"/lib/pkgconfig/fontconfig.pc || true
-fi
-if [ ! -s "$SDK_HOME/lib/pkgconfig/fontconfig.pc" ] || [ "$(pkg-config --modversion fontconfig)" != "$FONTCONFIG_VERSION" ]; then
-    start_build "$FONTCONFIG_TAR"
-    download "$FONTCONFIG_SITE" "$FONTCONFIG_TAR"
-    untar "$SRC_PATH/$FONTCONFIG_TAR"
-    pushd "fontconfig-${FONTCONFIG_VERSION}"
-    env CFLAGS="$BF" CXXFLAGS="$BF" ./configure --prefix="$SDK_HOME" --disable-docs --disable-static --enable-shared
-    make -j${MKJOBS}
-    make install
-    popd
-    rm -rf "fontconfig-${FONTCONFIG_VERSION}"
-    end_build "$FONTCONFIG_TAR"
-fi
-
 # Install libmount (required by glib)
 # see http://www.linuxfromscratch.org/lfs/view/development/chapter06/util-linux.html
 if [ "${RHEL_MAJOR:-7}" -le 6 ]; then
@@ -1393,6 +1370,30 @@ if [ ! -s "$SDK_HOME/lib/pkgconfig/mount.pc" ] || [ "$(pkg-config --modversion m
     rm -rf "util-linux-${UTILLINUX_VERSION}"
     end_build "$UTILLINUX_TAR"
 fi
+
+# Install fontconfig
+# see http://www.linuxfromscratch.org/blfs/view/cvs/general/fontconfig.html
+FONTCONFIG_VERSION=2.13.0
+FONTCONFIG_TAR="fontconfig-${FONTCONFIG_VERSION}.tar.gz"
+FONTCONFIG_SITE="https://www.freedesktop.org/software/fontconfig/release"
+if [ "${REBUILD_FONTCONFIG:-}" = "1" ]; then
+    rm -f "$SDK_HOME"/lib/libfontconfig* || true
+    rm -rf "$SDK_HOME"/include/fontconfig || true
+    rm -f "$SDK_HOME"/lib/pkgconfig/fontconfig.pc || true
+fi
+if [ ! -s "$SDK_HOME/lib/pkgconfig/fontconfig.pc" ] || [ "$(pkg-config --modversion fontconfig)" != "$FONTCONFIG_VERSION" ]; then
+    start_build "$FONTCONFIG_TAR"
+    download "$FONTCONFIG_SITE" "$FONTCONFIG_TAR"
+    untar "$SRC_PATH/$FONTCONFIG_TAR"
+    pushd "fontconfig-${FONTCONFIG_VERSION}"
+    env CFLAGS="$BF" CXXFLAGS="$BF" ./configure --prefix="$SDK_HOME" --disable-docs --disable-static --enable-shared
+    make -j${MKJOBS}
+    make install
+    popd
+    rm -rf "fontconfig-${FONTCONFIG_VERSION}"
+    end_build "$FONTCONFIG_TAR"
+fi
+
 
 # Install Texinfo (for gdb)
 # see http://www.linuxfromscratch.org/lfs/view/development/chapter06/texinfo.html
