e64cd7d15ad45930a78b200ddea3f2805211d263
Frédéric Devernay <frederic.devernay@m4x.org>
2019-06-17 20:33:55 -0700

    linux sdk: fix

--- tools/docker/natron-sdk-i386/build.sh
+++ tools/docker/natron-sdk-i386/build.sh
@@ -4,5 +4,10 @@ env GEN_DOCKERFILE32=1 ../../jenkins/include/scripts/build-Linux-sdk.sh> Dockerf
 cp  ../../jenkins/common.sh ../../jenkins/compiler-common.sh .
 (cd ../../jenkins/include/scripts; tar cf - build-Linux-sdk.sh pkg) | tar xf -
 (cd ../../jenkins; tar cf - include/patches) | tar xf -
-docker build -t natrongithub/i386/natron-sdk:latest .
-#docker build --no-cache -t natron-sdk:latest .
+docker build -t natrongithub/natron-sdk-i386:latest .
+#docker build --no-cache -t natrongithub/natron-sdk-i386:latest .
+echo "please execute:"
+echo "docker login"
+echo "docker tag natrongithub/natron-sdk-i386:latest natrongithub/natron-sdk-i386:$(date +%Y%m%d)"
+echo "docker push natrongithub/natron-sdk-i386:latest"
+echo "docker push natrongithub/natron-sdk-i386:$(date +%Y%m%d)"
