73c1e5dbaeba89990d14e17a01045ffb3bac11ff
Frédéric Devernay <frederic.devernay@m4x.org>
2019-07-20 11:19:53 -0700

    Update gen-natron-doc.sh

--- tools/jenkins/gen-natron-doc.sh
+++ tools/jenkins/gen-natron-doc.sh
@@ -55,7 +55,6 @@ if "$GENDOCS"; then
         #SPHINX_BIN="sphinx-build2.exe" # fails with 'failed to create process (\mingw64\bin\python2.exe "C:\msys64\mingw64\bin\sphinx-build2-script.py").'
         SPHINX_BIN="sphinx-build2-script.py"
     elif [ "$PKGOS" = "OSX" ]; then
-        RES_DIR="$TMP_PORTABLE_DIR/Contents/Resources"
         SPHINX_BIN="sphinx-build-${PYVER}"
     fi
     $SPHINX_BIN -b html source html
