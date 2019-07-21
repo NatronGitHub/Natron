56beab066d06d409aa4cf792b27f5533cd9b3333
Frédéric Devernay <frederic.devernay@m4x.org>
2019-07-21 08:51:07 -0700

    tools/docker: cosmetic changes

--- tools/docker/natron-sdk-i386/build.sh
+++ tools/docker/natron-sdk-i386/build.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 cd `dirname "$0"`
+LABEL="natrongithub/natron-sdk-i386"
 env GEN_DOCKERFILE32=1 ../../jenkins/include/scripts/build-Linux-sdk.sh> Dockerfile
 cp  ../../jenkins/common.sh ../../jenkins/compiler-common.sh .
 (cd ../../jenkins/include/scripts; tar cf - build-Linux-sdk.sh pkg) | tar xf -
 (cd ../../jenkins; tar cf - include/patches) | tar xf -
-docker build -t natrongithub/natron-sdk-i386:latest .
-#docker build --no-cache -t natrongithub/natron-sdk-i386:latest .
+docker build -t "${LABEL}:latest" .
+#docker build --no-cache -t "${LABEL}:latest" .
 echo "please execute:"
+#echo "docker-squash ${LABEL}:latest"
 echo "docker login"
-echo "docker tag natrongithub/natron-sdk-i386:latest natrongithub/natron-sdk-i386:$(date +%Y%m%d)"
-echo "docker push natrongithub/natron-sdk-i386:latest"
-echo "docker push natrongithub/natron-sdk-i386:$(date +%Y%m%d)"
+echo "docker tag ${LABEL}:latest ${LABEL}:$(date -u +%Y%m%d)"
+echo "docker push ${LABEL}:latest"
+echo "docker push ${LABEL}:$(date -u +%Y%m%d)"
