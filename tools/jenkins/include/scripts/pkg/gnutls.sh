c912cd3fd447fb243491b7fd7f36cacf7d6dee07
Frédéric Devernay <frederic.devernay@m4x.org>
2019-08-06 15:39:34 -0700

    linux sdk: downgrade gnutls to 3.6.8

--- tools/jenkins/include/scripts/pkg/gnutls.sh
+++ tools/jenkins/include/scripts/pkg/gnutls.sh
@@ -2,7 +2,13 @@
 
 # Install gnutls (for ffmpeg)
 # see http://www.linuxfromscratch.org/blfs/view/cvs/postlfs/gnutls.html
-GNUTLS_VERSION=3.6.9
+GNUTLS_VERSION=3.6.8
+# 3.6.9 fails on CentOS6 (probably needs an updated binutils/gas) with:
+#  CCAS     elf/sha512-ssse3-x86_64.lo
+#elf/sha1-ssse3-x86_64.s: Assembler messages:
+#elf/sha1-ssse3-x86_64.s:3825: Error: no such instruction: `vinserti128 $1,(%r13),%ymm0,%ymm0'
+#...
+
 GNUTLS_VERSION_MICRO= #.1
 GNUTLS_VERSION_SHORT=${GNUTLS_VERSION%.*}
 GNUTLS_TAR="gnutls-${GNUTLS_VERSION}${GNUTLS_VERSION_MICRO}.tar.xz"
