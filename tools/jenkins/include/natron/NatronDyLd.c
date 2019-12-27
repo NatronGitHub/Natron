2eda98d7a7315bcf55372d241f517b181517ca24
Frédéric Devernay <frederic.devernay@m4x.org>
2019-12-27 10:37:25 -0800

    NatronDyLd.c: force loading the provided libc++ on OS X < 10.8
    addresses https://github.com/NatronGitHub/Natron/issues/431

--- tools/jenkins/include/natron/NatronDyLd.c
+++ tools/jenkins/include/natron/NatronDyLd.c
@@ -9,6 +9,7 @@
 #include <limits.h> // PATH_MAX
 #include <spawn.h> // posix_spawn
 #include <err.h> // err
+#include <sys/sysctl.h> // sysctlbyname
 
 extern char** environ;
 
@@ -72,6 +73,62 @@ setup_spawnattr(posix_spawnattr_t* spawnattr)
   }
 }
 
+// from https://stackoverflow.com/a/11697362
+// (tested on Snow Leopard and Catalina)
+
+// Darwin version to OS X release:
+
+// 19.x.x  macOS 10.15.x Catalina
+// 18.x.x  macOS 10.14.x Mojave
+// 17.x.x. macOS 10.13.x High Sierra
+// 16.x.x  macOS 10.12.x Sierra
+// 15.x.x  OS X  10.11.x El Capitan
+// 14.x.x  OS X  10.10.x Yosemite
+// 13.x.x  OS X  10.9.x  Mavericks
+// 12.x.x  OS X  10.8.x  Mountain Lion
+// 11.x.x  OS X  10.7.x  Lion
+// 10.x.x  OS X  10.6.x  Snow Leopard
+//  9.x.x  OS X  10.5.x  Leopard
+//  8.x.x  OS X  10.4.x  Tiger
+//  7.x.x  OS X  10.3.x  Panther
+//  6.x.x  OS X  10.2.x  Jaguar
+//  5.x    OS X  10.1.x  Puma
+
+#ifdef LIBCXX
+/* kernel version as major minor component*/
+struct kern {
+  short int version[3];
+};
+
+/* return the kernel version */
+void GetKernelVersion(struct kern *k) {
+  static short int version_[3] = {0};
+  if (!version_[0]) {
+    // just in case it fails someday
+    version_[0] = version_[1] = version_[2] = -1;
+    char str[256] = {0};
+    size_t size = sizeof(str);
+    int ret = sysctlbyname("kern.osrelease", str, &size, NULL, 0);
+    if (ret == 0) sscanf(str, "%hd.%hd.%hd", &version_[0], &version_[1], &version_[2]);
+  }
+  memcpy(k->version, version_, sizeof(version_));
+}
+
+/* compare os version with a specific one
+   0 is equal
+   negative value if the installed version is less
+   positive value if the installed version is more
+*/
+int CompareKernelVersion(short int major, short int minor, short int component) {
+  struct kern k;
+  GetKernelVersion(&k);
+  if ( k.version[0] !=  major) return major - k.version[0];
+  if ( k.version[1] !=  minor) return minor - k.version[1];
+  if ( k.version[2] !=  component) return component - k.version[2];
+  return 0;
+}
+#endif
+
 int main(int argc, char *argv[])
 {
   char exec_path[PATH_MAX];
@@ -93,13 +150,18 @@ int main(int argc, char *argv[])
     exit(1);
   }
 #elif defined(LIBCXX)
-  /*
-   * set the DYLD_FALLBACK_LIBRARY_PATH environment variable to the directory containing only the libc++.1.dylib
-   * and re-exec the binary. The re-exec is necessary as the DYLD_FALLBACK_LIBRARY_PATH is only read at exec.
-   */
-  if (asprintf(&dyldLibraryPathDef, "DYLD_FALLBACK_LIBRARY_PATH=%s/../Frameworks/libcxx", programDir) == -1) {
-    fprintf(stderr, "Could not allocate space for defining DYLD_FALLBACK_LIBRARY_PATH environment variable\n");
-    exit(1);
+  // std::length_error::~length_error() is only defined in libc++abi.dylib on 10.9 (13.0.0) and later.
+  // see https://github.com/NatronGitHub/Natron/issues/431
+  // We this force linking with the provided libc++ on previous versions of OS X.
+  if (CompareKernelVersion(13.0.0) < 0) {
+    /*
+     * set the DYLD_LIBRARY_PATH environment variable to the directory containing only the libc++.1.dylib
+     * and re-exec the binary. The re-exec is necessary as the DYLD_LIBRARY_PATH is only read at exec.
+     */
+    if (asprintf(&dyldLibraryPathDef, "DYLD_LIBRARY_PATH=%s/../Frameworks/libcxx", programDir) == -1) {
+      fprintf(stderr, "Could not allocate space for defining DYLD_LIBRARY_PATH environment variable\n");
+      exit(1);
+    }
   }
 #else
 #error "define LIBGCC or LIBCXX"
