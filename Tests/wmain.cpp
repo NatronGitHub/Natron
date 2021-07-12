5f218701153109ce8c4669d7e3a3d696032f6a47
Frederic Devernay <frederic.devernay@m4x.org>
2021-07-11 19:33:34 -0700

    Update wmain.cpp

--- Tests/wmain.cpp
+++ Tests/wmain.cpp
@@ -23,6 +23,7 @@ GTEST_API_ int main(int argc, char **argv)
         args << QString::fromUtf8("--clear-cache");
         args << QString::fromUtf8("--clear-openfx-cache");
         args << QString::fromUtf8("--no-settings");
+        args << QString::fromUtf8("dummy.ntp");
         CLArgs cl(args, true);
         if (!manager.load(argc, 0, cl)) {
             printf("Failed to load AppManager\n");
