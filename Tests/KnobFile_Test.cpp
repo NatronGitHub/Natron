b50c22e6c9f0c1a8876229ef5153751a3afd76c2
Martin Reboredo <yakoyoku@gmail.com>
2021-11-29 20:10:53 -0300

    Renovate rng with QRandomGenerator

--- Tests/KnobFile_Test.cpp
+++ Tests/KnobFile_Test.cpp
@@ -30,6 +30,10 @@
 #include <QtCore/QString>
 #include <QtCore/QDir>
 
+#if QT_VERSION >= QT_VERSION_CHECK(5, 0, 0)
+#include <QRandomGenerator>
+#endif
+
 #include "Engine/FileSystemModel.h"
 #include "Engine/StandardPaths.h"
 
@@ -50,7 +54,11 @@ TEST(SequenceParsing, TestHashCharacter) {
     ///create temporary files as a sequence and try to read that sequence.
     QString tempPath = StandardPaths::writableLocation(StandardPaths::eStandardLocationTemp);
     QDir dir(tempPath);
+#if QT_VERSION >= QT_VERSION_CHECK(5, 0, 0)
+    QString dirName = QString::fromUtf8("NatronUnitTest") + QString::number( QRandomGenerator::global()->generate() );
+#else
     QString dirName = QString::fromUtf8("NatronUnitTest") + QString::number( qrand() );
+#endif
 
     dir.mkpath( QString::fromUtf8(".") );
     dir.mkdir(dirName);
@@ -221,7 +229,11 @@ TEST(SequenceParsing, TestPrintfLikeSyntax) {
     ///create temporary files as a sequence and try to read that sequence.
     QString tempPath = StandardPaths::writableLocation(StandardPaths::eStandardLocationTemp);
     QDir dir(tempPath);
+#if QT_VERSION >= QT_VERSION_CHECK(5, 0, 0)
+    QString dirName = QString::fromUtf8("NatronUnitTest") + QString::number( QRandomGenerator::global()->generate() );
+#else
     QString dirName = QString::fromUtf8("NatronUnitTest") + QString::number( qrand() );
+#endif
     dir.mkpath( QString::fromUtf8(".") );
     dir.mkdir(dirName);
     dir.cd(dirName);
@@ -329,7 +341,11 @@ TEST(SequenceParsing, TestViews) {
     ///create temporary files as a sequence and try to read that sequence.
     QString tempPath = StandardPaths::writableLocation(StandardPaths::eStandardLocationTemp);
     QDir dir(tempPath);
+#if QT_VERSION >= QT_VERSION_CHECK(5, 0, 0)
+    QString dirName = QString::fromUtf8("NatronUnitTest") + QString::number( QRandomGenerator::global()->generate() );
+#else
     QString dirName = QString::fromUtf8("NatronUnitTest") + QString::number( qrand() );
+#endif
 
     dir.mkpath( QString::fromUtf8(".") );
     dir.mkdir(dirName);
