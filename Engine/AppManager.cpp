395c2227a14e9aaaf2c5820b027240ff91046241
Frédéric Devernay <devernay@users.noreply.github.com>
2022-08-12 17:31:26 -0400

    fixes to build using cmake on macOS/Qt5 (#834)
    * fixes to build using cmake on macOS/Qt5
    
    * Use cmake's FindEXPAT for case-insensitive filesystems

--- Engine/AppManager.cpp
+++ Engine/AppManager.cpp
@@ -20,6 +20,7 @@
 // ***** BEGIN PYTHON BLOCK *****
 // from <https://docs.python.org/3/c-api/intro.html#include-files>:
 // "Since Python may define some pre-processor definitions which affect the standard headers on some systems, you must include Python.h before any standard headers are included."
+#undef Py_LIMITED_API  // Needed for PyRun_SimpleString, PyRun_String, Py_NoUserSiteDirectory
 #include <Python.h>
 // ***** END PYTHON BLOCK *****
 
