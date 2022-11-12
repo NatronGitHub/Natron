34bc7f3566b1c7b97256c536e1c9ffee354687cc
Frederic Devernay <frederic.devernay@m4x.org>
2022-11-12 12:42:42 -0800

    linux sdk: update python3
    sync python 2.7 with python2.7-2.7.18-27.fc38

--- tools/jenkins/include/scripts/pkg/python2.sh
+++ tools/jenkins/include/scripts/pkg/python2.sh
@@ -34,7 +34,7 @@ if build_step && { force_build || { [ ! -s "$SDK_HOME/lib/pkgconfig/python2.pc" 
     pushd "Python-${PY2_VERSION}"
     
     ##########################################################
-    ## Patches from python2.7-2.7.18-15.fc36 https://src.fedoraproject.org/rpms/python2.7
+    ## Patches from python2.7-2.7.18-27.fc38 https://src.fedoraproject.org/rpms/python2.7
 
     # 00351 # 1ae2a3db6d7af4ea973d1aee285e5fb9f882fdd0
     # Avoid infinite loop when reading specially crafted TAR files using the tarfile module
@@ -83,6 +83,17 @@ if build_step && { force_build || { [ ! -s "$SDK_HOME/lib/pkgconfig/python2.pc" 
     # but a warning is raised if parse_qs is used on input that contains ';'.
     patch -Np1 -i "$INC_PATH"/patches/python27/00359-CVE-2021-23336.patch
 
+    # 00361 # b86ebbab187c809f536620e2cbf1f370886979a9
+    # Make python2.7 compatible with OpenSSL 3.0.0
+    #
+    # Backported from python3.
+    #
+    # Based on https://github.com/tiran/cpython/tree/2.7.18-openssl3
+    if version_gt "$OPENSSL_VERSION" 1.99.9999; then
+        # breaks the build with openssl 1.1
+        patch -Np1 -i "$INC_PATH"/patches/python27/00361-openssl-3-compat.patch
+    fi
+
     # 00366 # e76b05ea3313854adf80e290c07d5b38fef606bb
     # CVE-2021-3733: Fix ReDoS in urllib AbstractBasicAuthHandler
     #
@@ -104,6 +115,62 @@ if build_step && { force_build || { [ ! -s "$SDK_HOME/lib/pkgconfig/python2.pc" 
     #
     # Backported from Python 3.
     patch -Np1 -i "$INC_PATH"/patches/python27/00368-CVE-2021-3737.patch
+
+    # 00372 # 0039a5762ac21255d6e4c2421a2159cbecfc95bd
+    # CVE-2021-4189: ftplib should not use the host from the PASV response
+    #
+    # Upstream: https://bugs.python.org/issue43285
+    #
+    # Backported from the python3 branch.
+    patch -Np1 -i "$INC_PATH"/patches/python27/00372-CVE-2021-4189.patch
+
+    # 00375 # 5488ab84d2447aa8df8b3502e76f151ac2488947
+    # Fix precision in test_distance (test.test_turtle.TestVec2D).
+    # See: https://bugzilla.redhat.com/show_bug.cgi?id=2038843
+    patch -Np1 -i "$INC_PATH"/patches/python27/00375-fix-test_distance-to-enable-Python-build-on-i686.patch
+
+    # 00377 # 53940f806f5e3451c9b77b44e2fc4d18b243156b
+    # CVE-2022-0391: urlparse does not sanitize URLs containing ASCII newline and tabs
+    #
+    # ASCII newline and tab characters are stripped from the URL.
+    #
+    # Upstream: https://bugs.python.org/issue43882
+    #
+    # Backported from Python 3.
+    patch -Np1 -i "$INC_PATH"/patches/python27/00377-CVE-2022-0391.patch
+
+    # 00378 # 32b7f6200303fb36d104ac8d2afa3afba1789c1b
+    # Support expat 2.4.5
+    #
+    # Curly brackets were never allowed in namespace URIs
+    # according to RFC 3986, and so-called namespace-validating
+    # XML parsers have the right to reject them a invalid URIs.
+    #
+    # libexpat >=2.4.5 has become strcter in that regard due to
+    # related security issues; with ET.XML instantiating a
+    # namespace-aware parser under the hood, this test has no
+    # future in CPython.
+    #
+    # References:
+    # - https://datatracker.ietf.org/doc/html/rfc3968
+    # - https://www.w3.org/TR/xml-names/
+    #
+    # Also, test_minidom.py: Support Expat >=2.4.5
+    #
+    # Upstream: https://bugs.python.org/issue46811
+    #
+    # Backported from Python 3.
+    patch -Np1 -i "$INC_PATH"/patches/python27/00378-support-expat-2-4-5.patch
+
+    # 00382 # 65796028be4b28aaf027963d6fb2ef4c219e73dc
+    # Make mailcap refuse to match unsafe filenames/types/params (GH-91993)
+    #
+    # Upstream: https://github.com/python/cpython/issues/68966
+    #
+    # Tracker bug: https://bugzilla.redhat.com/show_bug.cgi?id=2075390
+    #
+    # Backported from python3.
+    patch -Np1 -i "$INC_PATH"/patches/python27/00382-cve-2015-20107.patch
 
     # (New patches go here ^^^)
     # End of Fedora patches
