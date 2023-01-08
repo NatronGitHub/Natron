27e1c38b779b6473c59b474b41c444a8e5de0233
Ryan Wright <SIvanoFFDS@gmail.com>
2023-01-08 14:12:30 +0700

    Revert autoformatting

--- .github/workflows/gen_config.sh
+++ .github/workflows/gen_config.sh
@@ -21,28 +21,28 @@
 # config.pri
 if [[ ${RUNNER_OS} == "Linux" ]]; then
     # Ubuntu 12.04 precise doesn't have a pkg-config file for expat (expat.pc)
-    echo 'boost: LIBS += -lboost_serialization' >config.pri
-    echo 'expat: LIBS += -lexpat' >>config.pri
-    echo 'expat: PKGCONFIG -= expat' >>config.pri
+    echo 'boost: LIBS += -lboost_serialization' > config.pri
+    echo 'expat: LIBS += -lexpat' >> config.pri
+    echo 'expat: PKGCONFIG -= expat' >> config.pri
     # pyside and shiboken for python3 cannot be configured with pkg-config on Ubuntu 12.04LTS Precise
     # pyside and shiboken for python2 still need the extra QtCore and QtGui include directories
-    echo 'PYSIDE_PKG_CONFIG_PATH = $$system($$PYTHON_CONFIG --prefix)/lib/pkgconfig' >>config.pri
-    echo 'pyside: PKGCONFIG -= pyside' >>config.pri
-    echo 'pyside: INCLUDEPATH += $$system(env PKG_CONFIG_PATH=$$PYSIDE_PKG_CONFIG_PATH pkg-config --variable=includedir pyside)' >>config.pri
-    echo 'pyside: INCLUDEPATH += $$system(env PKG_CONFIG_PATH=$$PYSIDE_PKG_CONFIG_PATH pkg-config --variable=includedir pyside)/QtCore' >>config.pri
-    echo 'pyside: INCLUDEPATH += $$system(env PKG_CONFIG_PATH=$$PYSIDE_PKG_CONFIG_PATH pkg-config --variable=includedir pyside)/QtGui' >>config.pri
-    echo 'pyside: INCLUDEPATH += $$system(env PKG_CONFIG_PATH=$$PYSIDE_PKG_CONFIG_PATH pkg-config --variable=includedir QtGui)' >>config.pri
+    echo 'PYSIDE_PKG_CONFIG_PATH = $$system($$PYTHON_CONFIG --prefix)/lib/pkgconfig' >> config.pri
+    echo 'pyside: PKGCONFIG -= pyside' >> config.pri
+    echo 'pyside: INCLUDEPATH += $$system(env PKG_CONFIG_PATH=$$PYSIDE_PKG_CONFIG_PATH pkg-config --variable=includedir pyside)' >> config.pri
+    echo 'pyside: INCLUDEPATH += $$system(env PKG_CONFIG_PATH=$$PYSIDE_PKG_CONFIG_PATH pkg-config --variable=includedir pyside)/QtCore' >> config.pri
+    echo 'pyside: INCLUDEPATH += $$system(env PKG_CONFIG_PATH=$$PYSIDE_PKG_CONFIG_PATH pkg-config --variable=includedir pyside)/QtGui' >> config.pri
+    echo 'pyside: INCLUDEPATH += $$system(env PKG_CONFIG_PATH=$$PYSIDE_PKG_CONFIG_PATH pkg-config --variable=includedir QtGui)' >> config.pri
     #echo 'pyside: LIBS += -lpyside.cpython-32mu' >> config.pri
-    echo 'pyside: LIBS += -lpyside-python2.7' >>config.pri
-    # pyside doesn't have PySide::getWrapperForQObject on Ubuntu 12.04LTS Precise
-    echo 'pyside: DEFINES += PYSIDE_OLD' >>config.pri
+    echo 'pyside: LIBS += -lpyside-python2.7' >> config.pri
+    # pyside doesn't have PySide::getWrapperForQObject on Ubuntu 12.04LTS Precise 
+    echo 'pyside: DEFINES += PYSIDE_OLD' >> config.pri
     #echo 'shiboken: PKGCONFIG -= shiboken' >> config.pri
     #echo 'shiboken: INCLUDEPATH += $$system(pkg-config --variable=includedir shiboken)' >> config.pri
     #echo 'shiboken: LIBS += -lshiboken.cpython-32mu' >> config.pri
 elif [[ ${RUNNER_OS} == "Linux" ]]; then
-    echo 'boost: INCLUDEPATH += /usr/local/include' >config.pri
-    echo 'boost: LIBS += -L/usr/local/lib -lboost_serialization-mt -lboost_thread-mt -lboost_system-mt' >>config.pri
-    echo 'expat: PKGCONFIG -= expat' >>config.pri
-    echo 'expat: INCLUDEPATH += /usr/local/opt/expat/include' >>config.pri
-    echo 'expat: LIBS += -L/usr/local/opt/expat/lib -lexpat' >>config.pri
+    echo 'boost: INCLUDEPATH += /usr/local/include' > config.pri
+    echo 'boost: LIBS += -L/usr/local/lib -lboost_serialization-mt -lboost_thread-mt -lboost_system-mt' >> config.pri
+    echo 'expat: PKGCONFIG -= expat' >> config.pri
+    echo 'expat: INCLUDEPATH += /usr/local/opt/expat/include' >> config.pri
+    echo 'expat: LIBS += -L/usr/local/opt/expat/lib -lexpat' >> config.pri
 fi
