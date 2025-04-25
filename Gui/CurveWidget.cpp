ea2e02883f2c6df693587fcad1bfdc4cb9f4d386
cedric paillé <cedricpaille@gmail.com>
2025-04-25 16:08:11 +0200

    Attempt to fix undocking issue

--- Gui/CurveWidget.cpp
+++ Gui/CurveWidget.cpp
@@ -164,6 +164,7 @@ CurveWidget::initializeGL()
     // always running in the main thread
     assert( qApp && qApp->thread() == QThread::currentThread() );
     appPTR->initializeOpenGLFunctionsOnce();
+    makeCurrent();
 }
 
 void
