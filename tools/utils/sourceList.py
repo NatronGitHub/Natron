a4aca301d8b011fb892dfce483fb0b11bfd1b067
cedric paillé <cedricpaille@gmail.com>
2025-04-25 16:07:49 +0200

    Build libs install

--- tools/utils/sourceList.py
+++ tools/utils/sourceList.py
@@ -15,7 +15,7 @@ def list_typesystem_cpp_sources(typesystem, out):
     sources = [f"{package.lower()}_module_wrapper.cpp"]
     sources.extend([f"{typename.lower()}_wrapper.cpp" for typename in types])
 
-    return [os.path.normpath(os.path.join(out, package, f)) for f in sources]
+    return [os.path.normpath(os.path.join(out, package, f)).replace("\\", "/") for f in sources]
 
 
 if __name__ == "__main__":
